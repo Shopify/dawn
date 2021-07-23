fe75c022313baa9198c44aa0aea03a36a94ea0b1
Perry Raskin <perryraskin@gmail.com>
2021-07-23 13:06:19 -0400

    Generates cart properties object, appends to cart-add API body

--- assets/product-form.js
+++ assets/product-form.js
@@ -1,28 +1,43 @@
 class ProductForm extends HTMLElement {
   constructor() {
-    super();   
+    super();
 
-    this.form = this.querySelector('form');
-    this.form.addEventListener('submit', this.onSubmitHandler.bind(this));
-    this.cartNotification = document.querySelector('cart-notification');
+    this.form = this.querySelector("form");
+    this.form.addEventListener("submit", this.onSubmitHandler.bind(this));
+    this.cartNotification = document.querySelector("cart-notification");
   }
 
   onSubmitHandler(evt) {
     evt.preventDefault();
     this.cartNotification.setActiveElement(document.activeElement);
-    
+
     const submitButton = this.querySelector('[type="submit"]');
 
-    submitButton.setAttribute('disabled', true);
-    submitButton.classList.add('loading');
+    submitButton.setAttribute("disabled", true);
+    submitButton.classList.add("loading");
+    let parsedForm = JSON.parse(serializeForm(this.form));
+    let properties = {};
+    for (const key in parsedForm) {
+      const propertiesKeyMatch = key.substring(0, 11);
+      if (propertiesKeyMatch === "properties[") {
+        const startIndex = key.indexOf("[") + 1;
+        const endIndex = key.indexOf("]");
+        const propertyKey = key.substring(startIndex, endIndex);
+        const propertyValue = parsedForm[key];
+        properties[propertyKey] = propertyValue;
+      }
+    }
+    parsedForm.properties = properties;
 
     const body = JSON.stringify({
-      ...JSON.parse(serializeForm(this.form)),
-      sections: this.cartNotification.getSectionsToRender().map((section) => section.id),
-      sections_url: window.location.pathname
+      ...parsedForm,
+      sections: this.cartNotification
+        .getSectionsToRender()
+        .map((section) => section.id),
+      sections_url: window.location.pathname,
     });
 
-    fetch(`${routes.cart_add_url}`, { ...fetchConfig('javascript'), body })
+    fetch(`${routes.cart_add_url}`, { ...fetchConfig("javascript"), body })
       .then((response) => response.json())
       .then((parsedState) => {
         this.cartNotification.renderContents(parsedState);
@@ -31,10 +46,10 @@ class ProductForm extends HTMLElement {
         console.error(e);
       })
       .finally(() => {
-        submitButton.classList.remove('loading');
-        submitButton.removeAttribute('disabled');
+        submitButton.classList.remove("loading");
+        submitButton.removeAttribute("disabled");
       });
   }
 }
 
-customElements.define('product-form', ProductForm);
+customElements.define("product-form", ProductForm);
