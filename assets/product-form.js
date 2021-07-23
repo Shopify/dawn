23de732c4dd0f0c6265ba44139bfa8bd8a679c07
Perry Raskin <perryraskin@gmail.com>
2021-07-23 13:34:10 -0400

    Update product-form.js

--- assets/product-form.js
+++ assets/product-form.js
@@ -2,9 +2,9 @@ class ProductForm extends HTMLElement {
   constructor() {
     super();
 
-    this.form = this.querySelector("form");
-    this.form.addEventListener("submit", this.onSubmitHandler.bind(this));
-    this.cartNotification = document.querySelector("cart-notification");
+    this.form = this.querySelector('form');
+    this.form.addEventListener('submit', this.onSubmitHandler.bind(this));
+    this.cartNotification = document.querySelector('cart-notification');
   }
 
   onSubmitHandler(evt) {
@@ -13,15 +13,15 @@ class ProductForm extends HTMLElement {
 
     const submitButton = this.querySelector('[type="submit"]');
 
-    submitButton.setAttribute("disabled", true);
-    submitButton.classList.add("loading");
+    submitButton.setAttribute('disabled', true);
+    submitButton.classList.add('loading');
     let parsedForm = JSON.parse(serializeForm(this.form));
     let properties = {};
     for (const key in parsedForm) {
       const propertiesKeyMatch = key.substring(0, 11);
-      if (propertiesKeyMatch === "properties[") {
-        const startIndex = key.indexOf("[") + 1;
-        const endIndex = key.indexOf("]");
+      if (propertiesKeyMatch === 'properties[') {
+        const startIndex = key.indexOf('[') + 1;
+        const endIndex = key.indexOf(']');
         const propertyKey = key.substring(startIndex, endIndex);
         const propertyValue = parsedForm[key];
         properties[propertyKey] = propertyValue;
@@ -37,7 +37,7 @@ class ProductForm extends HTMLElement {
       sections_url: window.location.pathname,
     });
 
-    fetch(`${routes.cart_add_url}`, { ...fetchConfig("javascript"), body })
+    fetch(`${routes.cart_add_url}`, { ...fetchConfig('javascript'), body })
       .then((response) => response.json())
       .then((parsedState) => {
         this.cartNotification.renderContents(parsedState);
@@ -46,10 +46,10 @@ class ProductForm extends HTMLElement {
         console.error(e);
       })
       .finally(() => {
-        submitButton.classList.remove("loading");
-        submitButton.removeAttribute("disabled");
+        submitButton.classList.remove('loading');
+        submitButton.removeAttribute('disabled');
       });
   }
 }
 
-customElements.define("product-form", ProductForm);
+customElements.define('product-form', ProductForm);
