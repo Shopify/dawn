a8768b4275c8c5f86af9675bb0b2310b5e1cb4c3
Ludo Segura <ludo.segura@shopify.com>
2021-08-09 13:40:28 -0400

    add close button event

--- assets/share.js
+++ assets/share.js
@@ -4,6 +4,8 @@ class ShareButton extends DetailsDisclosure {
 
     this.elements = {
       shareButton: this.querySelector('button'),
+      copyButton: this.querySelector('.share-button__copy'),
+      closeButton: this.querySelector('.share-button__close'),
       successMessage: this.querySelector('[id^="ShareMessage"]'),
       urlInput: this.querySelector('input')
     }
@@ -14,18 +16,22 @@ class ShareButton extends DetailsDisclosure {
     } else {
       this.mainDetailsToggle.addEventListener('toggle', this.toggleDetails.bind(this));
       this.mainDetailsToggle.querySelector('.share-button__copy').addEventListener('click', this.copyToClipboard.bind(this));
+      this.mainDetailsToggle.querySelector('.share-button__close').addEventListener('click', this.close.bind(this));
     }
   }
 
   toggleDetails() {
     if (!this.mainDetailsToggle.open)
       this.elements.successMessage.classList.add('hidden');
+      this.elements.closeButton.classList.add('hidden');
   }
 
   copyToClipboard() {
     navigator.clipboard.writeText(this.elements.urlInput.value).then(() => {
       this.elements.successMessage.classList.remove('hidden');
       this.elements.successMessage.setAttribute('aria-hidden', false);
+      this.elements.closeButton.classList.remove('hidden');
+      this.elements.closeButton.focus();
 
       setTimeout(() => {
         this.elements.successMessage.setAttribute('aria-hidden', true);
