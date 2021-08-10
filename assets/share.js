e3b42c6acd763a1724d91e146fe27a7733b41c0e
Ludo Segura <ludo.segura@shopify.com>
2021-08-10 10:59:52 -0400

    added accessibility attribute + design tweaks

--- assets/share.js
+++ assets/share.js
@@ -4,6 +4,7 @@ class ShareButton extends DetailsDisclosure {
 
     this.elements = {
       shareButton: this.querySelector('button'),
+      shareSummary: this.querySelector('summary'),
       closeButton: this.querySelector('.share-button__close'),
       successMessage: this.querySelector('[id^="ShareMessage"]'),
       urlInput: this.querySelector('input')
@@ -13,16 +14,27 @@ class ShareButton extends DetailsDisclosure {
       this.elements.shareButton.classList.remove('hidden');
       this.elements.shareButton.addEventListener('click', () => { navigator.share({ url: document.location.href, title: document.title }) });
     } else {
+      this.addAccessibilityAttributes();
       this.mainDetailsToggle.addEventListener('toggle', this.toggleDetails.bind(this));
       this.mainDetailsToggle.querySelector('.share-button__copy').addEventListener('click', this.copyToClipboard.bind(this));
       this.mainDetailsToggle.querySelector('.share-button__close').addEventListener('click', this.close.bind(this));
     }
   }
 
+  addAccessibilityAttributes() {
+    this.elements.shareSummary.setAttribute('role', 'button');
+    this.elements.shareSummary.setAttribute('aria-expanded', 'false');
+    this.elements.shareSummary.setAttribute('aria-controls', this.id);
+  }
+
   toggleDetails() {
-    if (!this.mainDetailsToggle.open)
+    if (!this.mainDetailsToggle.open) {
       this.elements.successMessage.classList.add('hidden');
       this.elements.closeButton.classList.add('hidden');
+      this.elements.shareSummary.setAttribute('aria-expanded', 'false');
+    } else {
+      this.elements.shareSummary.setAttribute('aria-expanded', 'true');
+    }
   }
 
   copyToClipboard() {
