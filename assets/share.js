789c3d9516b54a7c2f17b413f69527d8ec74fa79
Ludo <ludo.segura@shopify.com>
2021-08-17 17:21:12 -0400

    Share button UI update (#349)

--- assets/share.js
+++ assets/share.js
@@ -4,6 +4,8 @@ class ShareButton extends DetailsDisclosure {
 
     this.elements = {
       shareButton: this.querySelector('button'),
+      shareSummary: this.querySelector('summary'),
+      closeButton: this.querySelector('.share-button__close'),
       successMessage: this.querySelector('[id^="ShareMessage"]'),
       urlInput: this.querySelector('input')
     }
@@ -12,24 +14,35 @@ class ShareButton extends DetailsDisclosure {
       this.elements.shareButton.classList.remove('hidden');
       this.elements.shareButton.addEventListener('click', () => { navigator.share({ url: document.location.href, title: document.title }) });
     } else {
+      this.addAccessibilityAttributes();
       this.mainDetailsToggle.addEventListener('toggle', this.toggleDetails.bind(this));
-      this.mainDetailsToggle.querySelector('button').addEventListener('click', this.copyToClipboard.bind(this));
+      this.mainDetailsToggle.querySelector('.share-button__copy').addEventListener('click', this.copyToClipboard.bind(this));
+      this.mainDetailsToggle.querySelector('.share-button__close').addEventListener('click', this.close.bind(this));
     }
   }
 
+  addAccessibilityAttributes() {
+    this.elements.shareSummary.setAttribute('role', 'button');
+    this.elements.shareSummary.setAttribute('aria-expanded', 'false');
+    this.elements.shareSummary.setAttribute('aria-controls', this.elements.shareSummary.nextElementSibling.id);
+  }
+
   toggleDetails() {
-    if (!this.mainDetailsToggle.open)
+    if (!this.mainDetailsToggle.open) {
       this.elements.successMessage.classList.add('hidden');
+      this.elements.successMessage.textContent = '';
+      this.elements.closeButton.classList.add('hidden');
+      this.elements.shareSummary.focus();
+    }
+    this.elements.shareSummary.setAttribute('aria-expanded', this.mainDetailsToggle.open);
   }
 
   copyToClipboard() {
     navigator.clipboard.writeText(this.elements.urlInput.value).then(() => {
       this.elements.successMessage.classList.remove('hidden');
-      this.elements.successMessage.setAttribute('aria-hidden', false);
-
-      setTimeout(() => {
-        this.elements.successMessage.setAttribute('aria-hidden', true);
-      }, 6000);
+      this.elements.successMessage.textContent = window.accessibilityStrings.shareSuccess;
+      this.elements.closeButton.classList.remove('hidden');
+      this.elements.closeButton.focus();
     });
   }
 }
