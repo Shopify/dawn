7d29adc6832f448016cfabe659c8e4b985b74574
tauthomas01 <658169+tauthomas01@users.noreply.github.com>
2021-08-30 10:58:46 -0400

    Add escape key functionality on facets filters (#474)
    * Add escape functionality on filters
    
    * replace onKey function on details-disclosure and filters to a common function
    
    * add blur event when tabbing
    
    * revert blur event
    
    * remove duplicate query
    
    Co-authored-by: martinamarien <martina.marien@shopify.com>

--- assets/global.js
+++ assets/global.js
@@ -75,6 +75,17 @@ function removeTrapFocus(elementToFocus = null) {
   if (elementToFocus) elementToFocus.focus();
 }
 
+function onKeyUpEscape(event) {
+  if (event.code.toUpperCase() !== 'ESCAPE') return;
+
+  const openDetailsElement = event.target.closest('details[open]');
+  if (!openDetailsElement) return;
+
+  const summaryElement = openDetailsElement.querySelector('summary');
+  openDetailsElement.removeAttribute('open');
+  summaryElement.focus();
+}
+
 class QuantityInput extends HTMLElement {
   constructor() {
     super();
