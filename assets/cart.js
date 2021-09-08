3b46e3c5e3aa468652e9a31ad887958385248fea
Ludo Segura <ludo.segura@shopify.com>
2021-09-08 17:21:54 -0400

    Cart quantity update issues

--- assets/cart.js
+++ assets/cart.js
@@ -27,6 +27,7 @@ class CartItems extends HTMLElement {
   }
 
   onChange(event) {
+    console.log(event);
     this.updateQuantity(event.target.dataset.index, event.target.value, document.activeElement.getAttribute('name'));
   }
 
