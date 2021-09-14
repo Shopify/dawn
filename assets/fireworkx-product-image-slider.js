e4b05b15adc4596ccb02e247157dabca63d39f10
JordyBukondaTshibangu <69379641+JordyBukondaTshibangu@users.noreply.github.com>
2021-09-14 15:08:13 +0200

    style : slider image styling on product detail page

---
+++ assets/fireworkx-product-image-slider.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 5,
+    slidesPerView: 2,
+
+    breakpoints: {
+      320: {
+        slidesPerView: 3,
+        spaceBetween: 2
+      },
+      375: {
+        slidesPerView: 4,
+        spaceBetween: 2
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 2
+      }
+    },
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 5,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+});
