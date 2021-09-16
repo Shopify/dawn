927e04eaaac8aba48730b1a4256eafe1fb89e774
Ludo Segura <ludo.segura@shopify.com>
2021-09-16 17:03:28 -0400

    first tests

--- assets/predictive-search.js
+++ assets/predictive-search.js
@@ -26,8 +26,22 @@ class PredictiveSearch extends HTMLElement {
     return this.input.value.trim();
   }
 
+  handleTouchMove(event) {
+    console.log(event.target)
+    const targetIsElement = event.target.classList.contains('predictive-search');
+    const targetIsPartOfList = this.querySelector('.predictive-search').contains(event.target);
+    if (!targetIsPartOfList) {
+      event.preventDefault()
+      console.log('prevent');
+    } else {
+      true;
+    };
+  }
+
   onChange() {
     const searchTerm = this.getQuery();
+
+    window.addEventListener('touchmove', this.handleTouchMove.bind(this), { passive: false});
 
     if (!searchTerm.length) {
       this.close(true);
@@ -51,11 +65,13 @@ class PredictiveSearch extends HTMLElement {
     } else {
       this.getSearchResults(searchTerm);
     }
-  } 
+  }
 
   onFocusOut() {
     setTimeout(() => {
-      if (!this.contains(document.activeElement)) this.close();
+      if (!this.contains(document.activeElement)) {
+        this.close();
+      }
     })
   }
 
@@ -88,7 +104,7 @@ class PredictiveSearch extends HTMLElement {
 
   switchOption(direction) {
     if (!this.getAttribute('open')) return;
-    
+
     const moveUp = direction === 'up';
     const selectedElement = this.querySelector('[aria-selected="true"]');
     const allElements = this.querySelectorAll('li');
@@ -96,7 +112,7 @@ class PredictiveSearch extends HTMLElement {
 
     if (moveUp && !selectedElement) return;
 
-    this.statusElement.textContent = ''; 
+    this.statusElement.textContent = '';
 
     if (!moveUp && selectedElement) {
       activeElement = selectedElement.nextElementSibling || allElements[0];
@@ -108,7 +124,7 @@ class PredictiveSearch extends HTMLElement {
 
     activeElement.setAttribute('aria-selected', true);
     if (selectedElement) selectedElement.setAttribute('aria-selected', false);
- 
+
     this.setLiveRegionText(activeElement.textContent);
     this.input.setAttribute('aria-activedescendant', activeElement.id);
   }
@@ -129,7 +145,7 @@ class PredictiveSearch extends HTMLElement {
     }
 
     fetch(`${routes.predictive_search_url}?q=${encodeURIComponent(searchTerm)}&${encodeURIComponent('resources[type]')}=product&${encodeURIComponent('resources[limit]')}=4&section_id=predictive-search`)
-      .then((response) => { 
+      .then((response) => {
         if (!response.ok) {
           var error = new Error(response.status);
           this.close();
@@ -146,7 +162,7 @@ class PredictiveSearch extends HTMLElement {
       .catch((error) => {
         this.close();
         throw error;
-      }); 
+      });
   }
 
   setLiveRegionLoadingState() {
@@ -160,7 +176,7 @@ class PredictiveSearch extends HTMLElement {
   setLiveRegionText(statusText) {
     this.statusElement.setAttribute('aria-hidden', 'false');
     this.statusElement.textContent = statusText;
-    
+
     setTimeout(() => {
       this.statusElement.setAttribute('aria-hidden', 'true');
     }, 1000);
@@ -168,16 +184,16 @@ class PredictiveSearch extends HTMLElement {
 
   renderSearchResults(resultsMarkup) {
     this.predictiveSearchResults.innerHTML = resultsMarkup;
-    this.setAttribute('results', true);  
+    this.setAttribute('results', true);
 
     this.setLiveRegionResults();
     this.open();
   }
 
-  setLiveRegionResults() { 
+  setLiveRegionResults() {
     this.removeAttribute('loading');
     this.setLiveRegionText(this.querySelector('[data-predictive-search-live-region-count-value]').textContent);
-  } 
+  }
 
   getResultsMaxHeight() {
     this.resultsMaxHeight = window.innerHeight - document.getElementById('shopify-section-header').getBoundingClientRect().bottom;
@@ -190,7 +206,7 @@ class PredictiveSearch extends HTMLElement {
     this.input.setAttribute('aria-expanded', true);
   }
 
-  close(clearSearchTerm = false) { 
+  close(clearSearchTerm = false) {
     if (clearSearchTerm) {
       this.input.value = '';
       this.removeAttribute('results');
@@ -205,6 +221,8 @@ class PredictiveSearch extends HTMLElement {
     this.input.setAttribute('aria-expanded', false);
     this.resultsMaxHeight = false
     this.predictiveSearchResults.removeAttribute('style');
+    console.log('close')
+    window.removeEventListener('touchmove', this.handleTouchMove.bind(this), { passive: false})
   }
 }
 
