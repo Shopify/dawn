849b1f849f82e3e931bcbfd483d453ca7cddca31
David Lanham <david@fireworkx.co.za>
2021-09-22 15:19:49 +0200

    Fireworkx product detail updates

--- assets/fireworkx-product-image-slider.js
+++ assets/fireworkx-product-image-slider.js
@@ -1,31 +1,30 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 4,
-    slidesPerView: 2,
-
-    breakpoints: {
-      320: {
-        slidesPerView: 3,
-        spaceBetween: 2
-      },
-      375: {
-        slidesPerView: 4,
-        spaceBetween: 2
-      },
-      768: {
-        slidesPerView: 4,
-        spaceBetween: 2
-      }
-    },
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-  var swiper2 = new Swiper(".mySwiper2", {
-    spaceBetween: 5,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: swiper,
-    },
+	spaceBetween: 10,
+	slidesPerView: 2,
+	breakpoints: {
+		320: {
+			slidesPerView: 3,
+			spaceBetween: 8
+		},
+		375: {
+			slidesPerView: 4,
+			spaceBetween: 8
+		},
+		768: {
+			slidesPerView: 4,
+			spaceBetween: 16
+		}
+	},
+	freeMode: true,
+	watchSlidesProgress: true
+});
+var swiper2 = new Swiper(".mySwiper2", {
+	spaceBetween: 10,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev"
+	},
+	thumbs: {
+		swiper: swiper
+	}
 });
