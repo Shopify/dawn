d3667785e283d5d4b9d0ace9ecc05652779d0419
JordyBukondaTshibangu <69379641+JordyBukondaTshibangu@users.noreply.github.com>
2021-09-22 12:53:36 +0200

    features : render form snippet for accessories and canopies

--- assets/fireworkx-quote-slider.js
+++ assets/fireworkx-quote-slider.js
@@ -12,8 +12,10 @@ var swiper = new Swiper(".mySwiper", {
 const buttons = document.querySelectorAll('.button-quote-type')
 const brands = document.querySelectorAll('li.brand-name')
 const products = document.querySelectorAll('li.product-name')
+const accessories = document.querySelectorAll('li.accessory-product')
+const singleProductList = document.querySelectorAll('li.single-product')
 
-buttons.forEach((button) => {
+buttons.forEach(button => {
   button.addEventListener('click', () => {
     const quoteType = button.dataset.quote;    
     if(quoteType === 'new-canopies'){
@@ -24,7 +26,7 @@ buttons.forEach((button) => {
   })
 })
 
-brands.forEach((brand) => {
+brands.forEach(brand => {
   brand.addEventListener('click', () => {
     let brandName = brand.dataset.brand
     sortListProducts(brandName);
@@ -37,4 +39,26 @@ function sortListProducts(brand){
   products.forEach(product => { product.classList.remove('show-product')})
   let filteredProducts = [... products].filter(product =>  product.dataset.vendor === brand)
   filteredProducts.forEach(product => { product.classList.add('show-product')})
+}
+
+products.forEach(product => {
+  product.addEventListener('click', () => {
+    const productTitle = product.dataset.title
+    showProductDetails(productTitle)
+    swiper.slideTo(4)
+  })
+})
+
+accessories.forEach(product => {
+  product.addEventListener('click', () => {
+    const productTitle = product.dataset.title
+    showProductDetails(productTitle)
+    swiper.slideTo(4)
+  })
+})
+
+function showProductDetails(productTitle){
+  singleProductList.forEach(singleProduct => { singleProduct.classList.remove('show-single-product')})
+  const products = [...singleProductList].filter(singleProduct => singleProduct.dataset.singleproduct === productTitle)
+  products.forEach(product => { product.classList.add('show-single-product')})
 }
