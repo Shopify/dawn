ed7dd9e04aa034d2227933a10d572b2d6eccaa2d
David Lanham <david@fireworkx.co.za>
2021-09-29 08:20:17 +0200

    More updates

--- assets/fireworkx-quote-slider.js
+++ assets/fireworkx-quote-slider.js
@@ -1,4 +1,9 @@
-var swiper = new Swiper(".fwx-enquiry-swiper", {
+let enquiryType = "",
+	collectionHandle = "",
+	productHandle = "";
+
+// Initialise swiper
+var enquirySwiper = new Swiper(".fwx-enquiry-swiper", {
 	allowTouchMove: false,
 	pagination: {
 		el: ".swiper-pagination",
@@ -9,100 +14,109 @@ var swiper = new Swiper(".fwx-enquiry-swiper", {
 	}
 });
 
-window.onload = () => {
-	// Modal
-	const body = document.querySelector("body"),
-		openModalElems = document.querySelectorAll('[data-modal="open-modal"]'),
-		closeModalElems = document.querySelectorAll([
-			'[data-modal="close-modal"]',
-			".fwx-slide-container"
-		]);
+// Modal
+const body = document.querySelector("body"),
+	modal = document.querySelector(".modal"),
+	openModalElems = document.querySelectorAll('[data-modal="open-modal"]'),
+	closeModalElems = document.querySelectorAll([
+		'[data-modal="close-modal"]',
+		".fwx-slide-container"
+	]);
 
-	if (openModalElems) {
-		openModalElems.forEach((elem) => {
-			elem.addEventListener("click", () => {
-				body.classList.add("modal-open");
-			});
+if (openModalElems) {
+	openModalElems.forEach((elem) => {
+		elem.addEventListener("click", () => {
+			body.classList.add("modal-open");
 		});
-	}
+	});
+}
 
-	if (closeModalElems) {
-		closeModalElems.forEach((elem) => {
-			elem.addEventListener("click", (e) => {
-				if (e.target === elem) {
-					body.classList.remove("modal-open");
-				}
-			});
-		});
-	}
-
-	// Enquiry Swiper
-
-	const enquiryChoiceButtons = document.querySelectorAll("[data-enquiry-type]"),
-		brands = document.querySelectorAll("li.brand-name"),
-		products = document.querySelectorAll("li.product-name"),
-		productsHeading = document.querySelector(".product-heading"),
-		accessories = document.querySelectorAll("li.accessory-product"),
-		singleProductList = document.querySelectorAll("li.single-product");
-
-	enquiryChoiceButtons.forEach((button) => {
-		button.addEventListener("click", () => {
-			const quoteType = button.dataset.enquiryType;
-			if (quoteType === "new-canopies") {
-				swiper.slideTo(1);
-			} else if (quoteType === "accessories") {
-				swiper.slideTo(3);
+if (closeModalElems) {
+	closeModalElems.forEach((elem) => {
+		elem.addEventListener("click", (e) => {
+			if (e.target === elem) {
+				body.classList.remove("modal-open");
+				// Reset swiper position when modal is closed
+				// Add/Remove transition event listener to avoid swipe events happening while modal is closing
+				const resetSwiper = function () {
+					enquirySwiper.slideTo(0);
+					modal.removeEventListener("transitionend", resetSwiper);
+				};
+				modal.addEventListener("transitionend", resetSwiper);
 			}
 		});
 	});
+}
 
-	brands.forEach((brand) => {
-		brand.addEventListener("click", () => {
-			let brandName = brand.dataset.brand;
-			sortListProducts(brandName);
-			swiper.slideTo(2);
-		});
+// Enquiry Swiper
+
+const enquiryChoiceButtons = document.querySelectorAll("[data-enquiry-type]"),
+	brands = document.querySelectorAll("li.brand-name"),
+	products = document.querySelectorAll("li.product-name"),
+	productsHeading = document.querySelector(".product-heading"),
+	accessories = document.querySelectorAll("li.accessory-product"),
+	singleProductList = document.querySelectorAll("li.single-product");
+
+// Choose enquiry type and
+enquiryChoiceButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		const quoteType = button.dataset.enquiryType;
+		enquiryType = quoteType;
+		if (quoteType === "new-canopies") {
+			enquirySwiper.slideTo(1);
+		}
+		if (quoteType === "accessories") {
+			window.location.href = "/collections/accessories?enquiry";
+		}
 	});
+});
 
-	function sortListProducts(brand) {
-		productsHeading.textContent = brand;
-		products.forEach((product) => {
-			product.classList.remove("show-product");
-		});
-		let filteredProducts = [...products].filter((product) => product.dataset.vendor === brand);
-		filteredProducts.forEach((product) => {
-			product.classList.add("show-product");
-		});
-		console.log(filteredProducts);
-	}
+brands.forEach((brand) => {
+	brand.addEventListener("click", () => {
+		let brandName = brand.dataset.brand;
+		window.location.href = `/collections/${brandName}?enquiry`;
+	});
+});
 
+function sortListProducts(brand) {
+	productsHeading.textContent = brand;
+	// Reset all products
 	products.forEach((product) => {
-		product.addEventListener("click", () => {
-			const productTitle = product.dataset.title;
-			showProductDetails(productTitle);
-			swiper.slideTo(4);
-		});
+		product.classList.remove("show-product");
 	});
-
-	accessories.forEach((product) => {
-		product.addEventListener("click", () => {
-			const productTitle = product.dataset.title;
-			showProductDetails(productTitle);
-			swiper.slideTo(4);
-		});
+	// Filter by brand
+	let filteredProducts = [...products].filter((product) => product.dataset.vendor === brand);
+	filteredProducts.forEach((product) => {
+		product.classList.add("show-product");
 	});
+}
 
-	let count = 0;
-	function showProductDetails(productTitle) {
-		singleProductList.forEach((singleProduct) => {
-			singleProduct.classList.remove("show-single-product");
-		});
-		const products = [...singleProductList].filter(
-			(singleProduct) => singleProduct.dataset.singleproduct === productTitle
-		);
-		console.log(products);
-		products.forEach((product) => {
-			product.classList.add("show-single-product");
-		});
-	}
-};
+products.forEach((product) => {
+	product.addEventListener("click", () => {
+		const productTitle = product.dataset.title;
+		showProductDetails(productTitle);
+		enquirySwiper.slideTo(4);
+	});
+});
+
+accessories.forEach((product) => {
+	product.addEventListener("click", () => {
+		const productTitle = product.dataset.title;
+		showProductDetails(productTitle);
+		enquirySwiper.slideTo(4);
+	});
+});
+
+let count = 0;
+function showProductDetails(productTitle) {
+	singleProductList.forEach((singleProduct) => {
+		singleProduct.classList.remove("show-single-product");
+	});
+	const products = [...singleProductList].filter(
+		(singleProduct) => singleProduct.dataset.singleProduct === productTitle
+	);
+	console.log(products);
+	products.forEach((product) => {
+		product.classList.add("show-single-product");
+	});
+}
