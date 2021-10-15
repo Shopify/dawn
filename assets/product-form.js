2d1c8321c9e2b4a3ce0a1857b7e29ec8d621bbf5
Lucas Lacerda <37168033+LucasLacerdaUX@users.noreply.github.com>
2021-10-15 13:40:28 -0400

    Support file upload line item properties on Product Forms and Cart Notifications (#745)

--- assets/product-form.js
+++ assets/product-form.js
@@ -12,7 +12,7 @@ if (!customElements.get('product-form')) {
     onSubmitHandler(evt) {
       evt.preventDefault();
       const submitButton = this.querySelector('[type="submit"]');
-      if (submitButton.classList.contains('loading')) return; 
+      if (submitButton.classList.contains('loading')) return;
 
       this.handleErrorMessage();
       this.cartNotification.setActiveElement(document.activeElement);
@@ -23,11 +23,12 @@ if (!customElements.get('product-form')) {
 
       const config = fetchConfig('javascript');
       config.headers['X-Requested-With'] = 'XMLHttpRequest';
-      config.body = JSON.stringify({
-        ...JSON.parse(serializeForm(this.form)),
-        sections: this.cartNotification.getSectionsToRender().map((section) => section.id),
-        sections_url: window.location.pathname
-      });
+      delete config.headers['Content-Type'];
+
+      const formData = new FormData(this.form);
+      formData.append('sections', this.cartNotification.getSectionsToRender().map((section) => section.id));
+      formData.append('sections_url', window.location.pathname);
+      config.body = formData;
 
       fetch(`${routes.cart_add_url}`, config)
         .then((response) => response.json())
