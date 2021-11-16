5823638221793d9a0d8b34236986ccbdb0bcdee1
Chris Shortreed <chris@illusive.ch>
2021-11-16 10:05:55 -0700

    Product Page CRO changes

---
+++ snippets/jsonMetafields/ProductDirections.php
@@ -0,0 +1,47 @@
+<?php
+$array = [
+    [
+        'title' => 'Refreshing Mist',
+        'media' => 'wwy477ctoq',
+        'steps' => [
+            [ "This product is contained within a mist type application" ],
+            [ "Give the Refreshing Mist a quick shake before use" ],
+            [ "Hold 12 inches away from your face and press down, it will release a fine mist." ],
+            [ "Do this 3-5 times make sure to cover your entire face and neck and immersive yourself with the calming lavender scent" ],
+            [ "Once you have finished spraying do not rub in, simply use a patting motion and let it soak into your skin" ],
+        ],
+    ],
+    [
+        'title' => 'Radiating Cream',
+        'media' => '7phfq3eupg',
+        'steps' => [
+            [ "This product is contained within an airtight container, when you first receive your product you need to press the top of the jar 2-3 times to bring the product to the surface (you will only need to do this once)" ],
+            [ "Give the Radiating cream a quick shake before use" ],
+            [ "Apply it by using 1 pump and massaging it into your face and neck using circular motion twice per day." ],
+            [ "Focus Application around eyes and troubled area" ],
+        ],
+    ],
+    [
+        'title' => 'Restoring Serum',
+        'media' => '81p3bz6yvz',
+        'steps' => [
+            [ "This product is contained within an airtight container, when you first receive your product you need to press the top of the jar 2-3 times to bring the product to the surface (you will only need to do this once)" ],
+            [ "Give the Restoring Serum a quick shake before use" ],
+            [ "Apply it by using 2-3 pumps and massaging it into your face and neck using your ring finger and a circular motion twice per day." ],
+            [ "Focus Application around eyes and troubled area" ],
+        ],
+    ],
+    [
+        'title' => 'Renewing Cleanser',
+        'media' => 'tbf0ry89lb',
+        'steps' => [
+            [ "Make sure your face and/or neck is already damp" ],
+            [ "Give the Renewing Cleanser a quick shake before use" ],
+            [ "Squeeze the bottle contents onto your fingertips, the amount the size of a dime should be enough" ],
+            [ "Work the gel cleanser into a lather" ],
+            [ "Using small circles gently massage onto your face and neck" ],
+            [ "Rinse with water" ],
+            [ "Dry your face without rubbing, use a patting motion to soak up excess moisture" ],
+        ],
+    ],
+];
