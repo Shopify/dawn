275b805782c4380fde2a61003fc07d92dce9b920
Chris Shortreed <chris@illusive.ch>
2021-11-26 16:10:06 -0700

    Added key ingredients tab

---
+++ ProductDirections.php
@@ -0,0 +1,103 @@
+<?php
+$array = [
+    [
+        'title'       => 'Renewing Cleanser',
+        'description' => "This pore-purifying cleanser is an all-time favorite, this refreshing gel is a 2-in-1 cleanser that removes impurities, dirt, and makeup without dehydrating your skin.",
+        'media'       => 'tbf0ry89lb',
+        'sections' => [
+            [
+                'title' => 'Key Ingredients',
+                'content' => [
+                    [
+                        'title' => 'Lactic Acid',
+                        'body'  => 'is a gentle way to help control oiliness and breakouts, and even helps repair acne scars. It also removes skin cells from the surface of the skin, leaving a brighter more even complexion.',
+                    ], [
+                        'title' => 'Hyaluronic Acid',
+                        'body'  => 'is a powerful humectant that plumps and retains moisture, for a more supple and youthful appearance',
+                    ], [
+                        'title' => 'Green Tea Extract',
+                        'body'  => 'helps diminish the signs of aging, calms irritation, evens out discoloration, diminishes the appearance of cellulite, soothes redness and swelling, repairs damage, exfoliates, decreases the appearance and occurrence of blackheads, eliminates dead skin, dirt, bacteria, and other pollutants on the skin’s surface, decreases puffiness, and reduces the appearance of dark circles.',
+                    ], [
+                        'title' => 'Marimoist',
+                        'body'  => 'is a hydrator and skin conditioner, it hydrates the skin by creating an ionic barrier forming a reservoir of bound water.',
+                    ], [
+                        'title' => 'Geranium oil',
+                        'body'  => 'is a hydrator and skin conditioner, it hydrates the skin by creating an ionic barrier forming a reservoir of bound water.',
+                    ], [
+                        'title' => 'Sweet Iris',
+                        'body'  => 'sweet iris leaves have the unique ability to encourage a tighter, firmer, more youthful look. At the same time, sweet iris has a moisturizing capability too, softening and plumping skin.',
+                    ],
+                ]
+            ],
+            [
+                'title' => 'How To Use',
+                'content' => [
+                    [ "Make sure your face and/or neck is already damp" ],
+                    [ "Give the Renewing Cleanser a quick shake before use" ],
+                    [ "Squeeze the bottle contents onto your fingertips, the amount the size of a dime should be enough" ],
+                    [ "Work the gel cleanser into a lather" ], [ "Using small circles gently massage onto your face and neck" ],
+                    [ "Rinse with water" ],
+                    [ "Dry your face without rubbing, use a patting motion to soak up excess moisture" ],
+                ],
+            ]
+        ]
+    ]
+     [
+        'title'       => 'Refreshing Mist',
+        'description' => "This alcohol-free pH balancing & resurfacing treatment contains natural and invigorating aromas that will send you into a world of relaxation. This refreshing mist is designed to absorb excess sebum, leaving your skin looking smooth and even.",
+        'media'       => 'wwy477ctoq',
+    'sections' => [
+            'Key Ingredients' => [
+                'Cypress Oil'  => 'Has antibacterial, antifungal, and antiviral properties and is also an astringent. Give you the appearance of tightened pores and reduce the presence of excess sebum.',
+                'Lavender Oil' => 'Works to kill bacteria, and this can prevent and heal acne breakouts. It can aid in skin lightening since it reduces inflammation. It can reduce discoloration, including dark spots. Free radicals are partly responsible for fine lines and wrinkles on the face. Lavender oil is full of antioxidants, which help protect you from the free radicals.',
+                'Rosemary Oil' => 'Has natural antiseptic properties, can help protect skin from sun damage',
+                'Geranium Oil' => 'Eliminates dead skin cells, tightens skin, promotes regeneration of new skin, it also helps diminish the signs of aging',
+            ], 'How To Use'   => [
+                [ "This product is contained within a mist type application" ],
+                [ "Give the Refreshing Mist a quick shake before use" ],
+                [ "Hold 12 inches away from your face and press down, it will release a fine mist." ],
+                [ "Do this 3-5 times make sure to cover your entire face and neck and immersive yourself with the calming lavender scent" ],
+                [ "Once you have finished spraying do not rub in, simply use a patting motion and let it soak into your skin" ],
+            ],
+
+        ],
+    ], [
+        'title'       => 'Restoring Serum',
+        'description' => "This serum will deliver dramatic results in just a couple of treatments. This serum helps reduce the look of dark spots (signs of hyperpigmentation), while our blend of ingredients we dubbed the ‘recharger’ tones and firms your skin throughout the day.",
+        'media'       => '81p3bz6yvz', 'sections' => [
+            'Key Ingredients' => [
+                'Geranium Rose Oil' => 'Eliminates dead skin cells, tightens skin, promotes regeneration of new skin, diminishes signs of aging.',
+                'Lavender Oil'      => 'Reduces acne, lightens skin, reduces wrinkles.',
+                'Rosemary Oil'      => 'Has natural antiseptic properties, can help protect skin from sun damage.',
+                'Cypress Oil '      => 'Soothes and calms inflamed skin, provides astringent properties for tissue healing and antibacterial support for the skin.',
+                'Orchistem '        => 'are orchid stem cells that enhance and improve communication between skin stem cells and fibroblasts that result in a global rejuvenating effect',
+                'Capucine'          => 'Cleanse and Purify the Skin, decongests the pores by stimulating healthy cell turnover revealing a younger, smoother complexion. It visually changes the surface condition of your skin while increasing cell turn over rate for a healthy glow',
+                'Riboxyl '          => 'Helps eliminate crows feet (wrinkles around your eyes)',
+                'Vitasource '       => 'Delays the ageing of cells, while increasing cell duplication by 10%, it also helps improve skin firmness and elasticity.',
+                'Seborilys '        => 'is a multifunctional sebum regulator derived entirely from plant sources. It limits sebum production, treats greasy skin, and limits skin irritation.',
+            ], 'How To Use'   => [
+                [ "This product is contained within an airtight container, when you first receive your product you need to press the top of the jar 2-3 times to bring the product to the surface (you will only need to do this once)" ],
+                [ "Give the Restoring Serum a quick shake before use" ],
+                [ "Apply it by using 2-3 pumps and massaging it into your face and neck using your ring finger and a circular motion twice per day." ],
+                [ "Focus Application around eyes and troubled area" ],
+            ],
+        ], [
+            'title'       => 'Radiating Cream',
+            'description' => "The perfect do-it-all day & night moisturizer. This lightweight mega-moisturizer has clinical-grade ingredients that visibly brightens and delivers intense hydration for bright, bouncy skin.",
+            'media'       => '7phfq3eupg', 'sections' => [
+                'Key Ingredients' => [
+                    'Gatuline Skin Repair Bio' => 'stimulates optimal reconstruction for dry or damaged skin. It promotes natural regeneration for fresher, smoother & more radiant skin. Restores skin barrier function, it also preserves the skin from dehydration.',
+                    'Slimagine'                => 'active slimming ingredient with anti yo-yo effect, it reduces the storage of body fat within the face.',
+                    'Chufa Milk'               => 'nourishes, softens, provides elasticity and helps to maintain fantastic skin hydration.',
+                    'Hyaluronic Acid'          => 'is a powerful humectant that plumps and retains moisture, for a more supple and youthful appearance',
+                    'NaturePep Pea'            => 'visibly reduces dark spots, red spots & hyperpigmentation for a more uniform & even complexion',
+                    'Tonka Bean Extract'       => 'acts as an optical brightener, reducing appearance of uneven skin tone.',
+                    'Geranium Rose Oil'        => 'eliminates dead skin cells, tightens skin, promotes regeneration of new skin, and diminishes signs of aging.',
+                ], 'How To Use'   => [
+                    [ "This product is contained within an airtight container, when you first receive your product you need to press the top of the jar 2-3 times to bring the product to the surface (you will only need to do this once)" ],
+                    [ "Give the Radiating cream a quick shake before use" ],
+                    [ "Apply it by using 1 pump and massaging it into your face and neck using circular motion twice per day." ],
+                    [ "Focus Application around eyes and troubled area" ],
+                ],
+            ],
+        ];
