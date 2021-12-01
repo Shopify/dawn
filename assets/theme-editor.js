645faa2eb9408e3f015e8c3612d973d7727b7d85
Ludo Segura <ludo.segura@shopify.com>
2021-12-01 16:58:33 -0500

    fix text align and load the right slide when editing a block in the editor

---
+++ assets/theme-editor.js
@@ -0,0 +1,22 @@
+document.addEventListener('shopify:block:select', function(event) {
+  console.log('block selected')
+  const blockSelectedIsSlide = event.target.classList.contains('slideshow__slide');
+  if (!blockSelectedIsSlide) return;
+
+  const parentSlideshowComponent = event.target.closest('slideshow-component');
+  parentSlideshowComponent.pause();
+
+  const slideScrollPosition = parentSlideshowComponent.slider.scrollLeft + parentSlideshowComponent.sliderLastItem.clientWidth * (parentSlideshowComponent.sliderItemsToShow.indexOf(event.target) + 1 - parentSlideshowComponent.currentPage);
+  setTimeout(function() {
+    parentSlideshowComponent.slider.scrollTo({
+      left: slideScrollPosition
+    });
+  }, 200);
+});
+
+document.addEventListener('shopify:block:deselect', function(event) {
+  const blockDeselectedIsSlide = event.target.classList.contains('slideshow__slide');
+  if (!blockDeselectedIsSlide) return;
+  const parentSlideshowComponent = event.target.closest('slideshow-component');
+  if (parentSlideshowComponent.autoplayButtonIsSetToPlay) parentSlideshowComponent.play();
+});
