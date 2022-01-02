fedbf1a5722545f195c81d3e3a757b16dd9b2ec6
jericmontana <mntnjrc@gmail.com>
2022-01-02 08:17:10 +0800

    ShopBy | Wishlist

--- assets/wishlist.js
+++ assets/wishlist.js
@@ -6,7 +6,7 @@ const GRID_LOADED_CLASS = 'loaded';
 const selectors = {
   button: '[button-wishlist]',
   grid: '[grid-wishlist]',
-  productCard: '.grid__item',
+  productCard: '.card-wrapper',
 };
 
 document.addEventListener('DOMContentLoaded', () => {
