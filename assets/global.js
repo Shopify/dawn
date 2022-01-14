ee22e305142e1b5b1120b8eb83c1fcada7fdf836
Tyler Alsbury <60230011+tyleralsbury@users.noreply.github.com>
2022-01-14 13:55:09 -0500

    Fix for sticky menu close (#1171)
    * Fix for sticky menu close

--- assets/global.js
+++ assets/global.js
@@ -408,6 +408,7 @@ class HeaderDrawer extends MenuDrawer {
     this.header = this.header || document.getElementById('shopify-section-header');
     this.borderOffset = this.borderOffset || this.closest('.header-wrapper').classList.contains('header-wrapper--border-bottom') ? 1 : 0;
     document.documentElement.style.setProperty('--header-bottom-position', `${parseInt(this.header.getBoundingClientRect().bottom - this.borderOffset)}px`);
+    this.header.classList.add('menu-open');
 
     setTimeout(() => {
       this.mainDetailsToggle.classList.add('menu-opening');
@@ -416,6 +417,11 @@ class HeaderDrawer extends MenuDrawer {
     summaryElement.setAttribute('aria-expanded', true);
     trapFocus(this.mainDetailsToggle, summaryElement);
     document.body.classList.add(`overflow-hidden-${this.dataset.breakpoint}`);
+  }
+
+  closeMenuDrawer(event, elementToFocus) {
+    super.closeMenuDrawer(event, elementToFocus);
+    this.header.classList.remove('menu-open');
   }
 }
 
