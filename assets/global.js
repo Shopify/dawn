67d5196f50610dc6798629b8dc9f847fa1fa95a6
Ken Meleta <ken.meleta@shopify.com>
2022-02-02 10:10:21 -0700

    Re-simplify approach

--- assets/global.js
+++ assets/global.js
@@ -531,35 +531,12 @@ class SliderComponent extends HTMLElement {
     this.nextButton.addEventListener('click', this.onButtonClick.bind(this));
   }
 
-  getWaypoints() {
-    const waypoints = [];
-    for (let i = 0; i < this.totalPages; i++) {
-      if (i < this.totalPages - 1) {
-        waypoints.push(this.sliderItemsToShow[i].offsetLeft);
-      } else {
-        waypoints.push(this.slider.scrollWidth - this.slider.clientWidth);
-      }
-    };
-    return waypoints;
-  }
-
-  setNearestPage(x) {
-    const buffer = this.sliderLastItem.clientWidth / 2;
-    for (let i = 0; i < this.waypoints.length; i++) {
-      if (x < this.waypoints[i] + buffer && x > this.waypoints[i] - buffer) {
-        this.currentPage = i + 1;
-        break;
-      }
-    }
-  }
-
   initPages() {
     this.sliderItemsToShow = Array.from(this.sliderItems).filter(element => element.clientWidth > 0);
-    this.sliderLastItem = this.sliderItemsToShow[this.sliderItemsToShow.length - 1];
-    if (this.sliderItemsToShow.length === 0) return;
-    this.slidesPerPage = Math.floor(this.slider.clientWidth / this.sliderItemsToShow[0].clientWidth);
+    if (this.sliderItemsToShow.length < 2) return;
+    this.sliderItemOffset = this.sliderItemsToShow[1].offsetLeft - this.sliderItemsToShow[0].offsetLeft;
+    this.slidesPerPage = Math.floor(this.slider.clientWidth / this.sliderItemOffset);
     this.totalPages = this.sliderItemsToShow.length - this.slidesPerPage + 1;
-    this.waypoints = this.getWaypoints();
     this.update();
   }
 
@@ -570,7 +547,7 @@ class SliderComponent extends HTMLElement {
 
   update() {
     const previousPage = this.currentPage;
-    this.setNearestPage(this.slider.scrollLeft);
+    this.currentPage = Math.round(this.slider.scrollLeft / this.sliderItemOffset) + 1;
 
     if (this.currentPageElement && this.pageTotalElement) {
       this.currentPageElement.textContent = this.currentPage;
@@ -592,7 +569,7 @@ class SliderComponent extends HTMLElement {
       this.prevButton.removeAttribute('disabled');
     }
 
-    if (this.isSlideVisible(this.sliderLastItem)) {
+    if (this.isSlideVisible(this.sliderItemsToShow[this.sliderItemsToShow.length - 1])) {
       this.nextButton.setAttribute('disabled', 'disabled');
     } else {
       this.nextButton.removeAttribute('disabled');
@@ -607,7 +584,7 @@ class SliderComponent extends HTMLElement {
   onButtonClick(event) {
     event.preventDefault();
     const step = event.currentTarget.dataset.step || 1;
-    this.slideScrollPosition = event.currentTarget.name === 'next' ? this.slider.scrollLeft + (step * this.sliderLastItem.clientWidth) : this.slider.scrollLeft - (step * this.sliderLastItem.clientWidth);
+    this.slideScrollPosition = event.currentTarget.name === 'next' ? this.slider.scrollLeft + (step * this.sliderItemOffset) : this.slider.scrollLeft - (step * this.sliderItemOffset);
     this.slider.scrollTo({
       left: this.slideScrollPosition
     });
