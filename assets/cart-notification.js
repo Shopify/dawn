104d0e6d49af6d0a411844729aed614fc6d31c75
HOSein KH <hkh659@gmail.com>
2022-02-04 12:57:05 +0330

    #260: add cart-drawer for dawn

--- assets/cart-notification.js
+++ assets/cart-notification.js
@@ -2,72 +2,109 @@ class CartNotification extends HTMLElement {
   constructor() {
     super();
 
-    this.notification = document.getElementById('cart-notification');
-    this.header = document.querySelector('sticky-header');
-    this.onBodyClick = this.handleBodyClick.bind(this);
+    /* open cart drawer instead of notification
+    this.notification = document.getElementById("cart-notification");*/
+    this.notification = document.getElementById("cart-drawer");
     
-    this.notification.addEventListener('keyup', (evt) => evt.code === 'Escape' && this.close());
-    this.querySelectorAll('button[type="button"]').forEach((closeButton) =>
-      closeButton.addEventListener('click', this.close.bind(this))
+    this.header = document.querySelector("sticky-header");
+
+    this.onBodyClick = this.handleBodyClick.bind(this);
+
+    this.notification.addEventListener(
+      "keyup",
+      (evt) => evt.code === "Escape" && this.close()
     );
+    /* need to be editable after drawer shown, so i commented this section
+
+    this.querySelectorAll('button[type="button"]').forEach((closeButton) =>
+      closeButton.addEventListener("click", this.close.bind(this))
+    ); */
   }
 
   open() {
-    this.notification.classList.add('animate', 'active');
-
-    this.notification.addEventListener('transitionend', () => {
-      this.notification.focus();
-      trapFocus(this.notification);
-    }, { once: true });
-
-    document.body.addEventListener('click', this.onBodyClick);
+    /* open cart drawer instead of notification
+    this.notification.classList.add("animate", "active");
+    
+    
+    this.notification.addEventListener(
+      "transitionend",
+      () => {
+        this.notification.focus();
+        
+        trapFocus(this.notification);
+      },
+      { once: true }
+      );
+      
+      document.body.addEventListener("click", this.onBodyClick);
+      */
+   this.notification.classList.remove("is-hidden");
+   document.body.classList.add('is-drawer-opened');
   }
 
   close() {
-    this.notification.classList.remove('active');
+    this.notification.classList.remove("active");
 
-    document.body.removeEventListener('click', this.onBodyClick);
+    document.body.removeEventListener("click", this.onBodyClick);
 
     removeTrapFocus(this.activeElement);
   }
 
   renderContents(parsedState) {
-      this.productId = parsedState.id;
-      this.getSectionsToRender().forEach((section => {
-        document.getElementById(section.id).innerHTML =
-          this.getSectionInnerHTML(parsedState.sections[section.id], section.selector);
-      }));
+    this.productId = parsedState.id;
 
-      if (this.header) this.header.reveal();
-      this.open();
+    this.getSectionsToRender().forEach((section) => {
+      document.getElementById(section.id).innerHTML = this.getSectionInnerHTML(
+        parsedState.sections[section.id],
+        section.selector
+      );
+    });
+
+    this.header?.reveal();
+
+    this.open();
   }
 
   getSectionsToRender() {
     return [
       {
-        id: 'cart-notification-product',
+        id: "cart-notification-product",
+
         selector: `#cart-notification-product-${this.productId}`,
       },
+      
       {
-        id: 'cart-notification-button'
+        id: "cart-notification-button",
+      },
+      
+      {
+        id: "cart-icon-bubble",
       },
       {
-        id: 'cart-icon-bubble'
-      }
+        id: 'cart-drawer',
+        selector: '#cart-drawer',
+      },
     ];
   }
 
-  getSectionInnerHTML(html, selector = '.shopify-section') {
+  getSectionInnerHTML(html, selector = ".shopify-section") {
     return new DOMParser()
-      .parseFromString(html, 'text/html')
+
+      .parseFromString(html, "text/html")
+
       .querySelector(selector).innerHTML;
   }
 
   handleBodyClick(evt) {
     const target = evt.target;
-    if (target !== this.notification && !target.closest('cart-notification')) {
-      const disclosure = target.closest('details-disclosure');
-      this.activeElement = disclosure ? disclosure.querySelector('summary') : null;
+
+    if (target !== this.notification && !target.closest("cart-notification")) {
+      const disclosure = target.closest("details-disclosure");
+
+      this.activeElement = disclosure
+        ? disclosure.querySelector("summary")
+        : null;
+
       this.close();
     }
   }
@@ -77,4 +114,4 @@ class CartNotification extends HTMLElement {
   }
 }
 
-customElements.define('cart-notification', CartNotification);
+customElements.define("cart-notification", CartNotification);
