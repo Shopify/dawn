799c7f4354ee8308ffd283fcd0561366780d098d
Sofia Matulis <sofiamatulis@Sofias-MacBook-Pro-3.local>
2022-03-25 16:19:37 -0400

    Fix mobile bug drawer pills

--- assets/facets.js
+++ assets/facets.js
@@ -186,8 +186,17 @@ class FacetFiltersForm extends HTMLElement {
     } else {
       const forms = [];
       sortFilterForms.forEach((form) => {
-        if (form.id == 'FacetSortForm' || form.id == 'FacetFiltersForm' || form.id == 'FacetSortDrawerForm' || form.id == 'FacetFiltersFormMobile' ) forms.push(this.createSearchParams(form))})
-        this.onSubmitForm(forms.join('&'), event)
+        if (window.innerWidth >= 750) {
+          if (form.id == 'FacetSortForm' || form.id == 'FacetFiltersForm' || form.id == 'FacetSortDrawerForm') {
+            forms.push(this.createSearchParams(form))
+          }
+        } else {
+          if (form.id == 'FacetFiltersFormMobile') {
+            forms.push(this.createSearchParams(form))
+          }
+        }
+      })
+      this.onSubmitForm(forms.join('&'), event)
     }
   }
 
