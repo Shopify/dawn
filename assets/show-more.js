685d0e8f9cc73acec8bfe9a44251ad0b20ec31ea
Sofia Matulis <sofiamatulis@Sofias-MacBook-Pro-3.local>
2022-03-25 15:16:23 -0400

    clean up

--- assets/show-more.js
+++ assets/show-more.js
@@ -3,10 +3,10 @@ class ShowMoreButton extends HTMLElement {
     super();
     this.addEventListener('click', (event) => {
       if (event.target.nodeName === 'SPAN' && event.target.closest('[id^="Show-More-"]')) {
-        this.expandShowMore(event.target.closest('[id^="Show-More-"]'));
+        this.expandShowMore(event);
       }
       if (event.target.nodeName === 'BUTTON' && event.target.closest('[id^="Show-More-"]')) {
-        this.expandShowMore(event.target.closest('[id^="Show-More-"]'));
+        this.expandShowMore(event);
         const nextElementToFocus = event.target.parentNode.parentNode.querySelector(".show-more-item")
         if (!nextElementToFocus.classList.contains('.hidden')) {
           nextElementToFocus.querySelector('input').focus()
@@ -14,8 +14,8 @@ class ShowMoreButton extends HTMLElement {
       }
     });
   }
-  expandShowMore(showMore) {
-    const parentDisplay = showMore.closest('.parent-display');
+  expandShowMore(event) {
+    const parentDisplay = event.target.closest('[id^="Show-More-"]').closest('.parent-display');
     const parentWrap = parentDisplay.querySelector('.parent-wrap');
     parentWrap.classList.toggle('show-more-wrap');
     parentDisplay.querySelectorAll('.show-more-item').forEach(item => item.classList.toggle('hidden'))
