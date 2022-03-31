4d0d8a73d50e7e52980d12d94dc147716f0f0abf
LucasLacerdaUX <lucas.lacerda@shopify.com>
2022-03-31 15:21:45 -0400

    :facepalm:

--- assets/details-disclosure.js
+++ assets/details-disclosure.js
@@ -43,7 +43,7 @@ class HeaderMenu extends DetailsDisclosure {
     this.header.preventHide = !!this.querySelectorAll('.mega-menu[open]').length;
 
     if (document.documentElement.style.getPropertyValue('--header-bottom-position-desktop') !== '') return;
-    document.documentElement.style.setProperty('--header-bottom-position-desktop', `${Math.floor(this.header.getBoundingClientRect().bottom)}px`);    
+    document.documentElement.style.setProperty('--header-bottom-position-desktop', `${Math.floor(this.header.getBoundingClientRect().bottom)}px`);
   }
 }
 
