51defb685c0dfd1c12d40855c893b238569e0e29
Lucas Lacerda <37168033+LucasLacerdaUX@users.noreply.github.com>
2022-03-31 17:09:37 -0400

    Prevent auto-hiding on dropdown menus

--- assets/details-disclosure.js
+++ assets/details-disclosure.js
@@ -40,7 +40,7 @@ class HeaderMenu extends DetailsDisclosure {
 
   onToggle() {
     if (!this.header) return;
-    this.header.preventHide = !!this.querySelectorAll('.mega-menu[open]').length;
+    this.header.preventHide = this.mainDetailsToggle.open;
 
     if (document.documentElement.style.getPropertyValue('--header-bottom-position-desktop') !== '') return;
     document.documentElement.style.setProperty('--header-bottom-position-desktop', `${Math.floor(this.header.getBoundingClientRect().bottom)}px`);
