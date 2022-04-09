2e72eaa8f589be62e46d9fcde304e7f9e1f4950d
Sav Tripodi <42227195+savtrip@users.noreply.github.com>
2022-04-09 12:19:43 +0200

    Added missing semi colon in global.js
    Was reading the code and noticed a missing semi colon for terminating the end of a line of JavaScript.

--- assets/global.js
+++ assets/global.js
@@ -837,7 +837,7 @@ class VariantSelects extends HTMLElement {
     fetch(`${this.dataset.url}?variant=${this.currentVariant.id}&section_id=${this.dataset.originalSection ? this.dataset.originalSection : this.dataset.section}`)
       .then((response) => response.text())
       .then((responseText) => {
-        const html = new DOMParser().parseFromString(responseText, 'text/html')
+        const html = new DOMParser().parseFromString(responseText, 'text/html');
         const destination = document.getElementById(`price-${this.dataset.section}`);
         const source = html.getElementById(`price-${this.dataset.originalSection ? this.dataset.originalSection : this.dataset.section}`);
         if (source && destination) destination.innerHTML = source.innerHTML;
