a17f2aafce2693372ad63b497ad82f53df867ce8
Ludo Segura <ludo.segura@shopify.com>
2022-04-13 12:20:38 +0200

    Visual tweaks, spacing and a11y fix for the cart note and duplicate IDs

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -8,13 +8,17 @@ class CartDrawer extends HTMLElement {
 
   open(triggeredBy) {
     if (triggeredBy) this.setActiveElement(triggeredBy);
+    const cartDrawerNote = this.querySelector('[id^="Details-"] summary');
+    if (cartDrawerNote && !cartDrawerNote.hasAttribute('role')) this.setSummaryAccessibility(cartDrawerNote);
     this.classList.add('animate', 'active');
+
     this.addEventListener('transitionend', () => {
       this.focus();
       const cartDrawer = document.getElementById('cart-drawer');
       const focusElement = cartDrawer.querySelector('.drawer__inner') || cartDrawer.querySelector('.drawer__close');
       trapFocus(cartDrawer, focusElement);
     }, { once: true });
+
     document.body.classList.add(`overflow-hidden`);
   }
 
@@ -22,6 +26,21 @@ class CartDrawer extends HTMLElement {
     this.classList.remove('active');
     removeTrapFocus(this.activeElement);
     document.body.classList.remove(`overflow-hidden`);
+  }
+
+  setSummaryAccessibility(cartDrawerNote) {
+    cartDrawerNote.setAttribute('role', 'button');
+    cartDrawerNote.setAttribute('aria-expanded', 'false');
+
+    if(cartDrawerNote.nextElementSibling.getAttribute('id')) {
+      cartDrawerNote.setAttribute('aria-controls', cartDrawerNote.nextElementSibling.id);
+    }
+
+    cartDrawerNote.addEventListener('click', (event) => {
+      event.currentTarget.setAttribute('aria-expanded', !event.currentTarget.closest('details').hasAttribute('open'));
+    });
+
+    cartDrawerNote.parentElement.addEventListener('keyup', onKeyUpEscape);
   }
 
   renderContents(parsedState) {
