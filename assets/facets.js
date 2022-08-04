7a05ef009d16d029c20bb6f403ae14e93377a8ee
Sofia Matulis <sofiamatulis@users.noreply.github.com>
2022-08-04 10:57:27 -0400

    Fix filter bug to allow filters to be removed (#1890)

--- assets/facets.js
+++ assets/facets.js
@@ -184,6 +184,8 @@ class FacetFiltersForm extends HTMLElement {
       sortFilterForms.forEach((form) => {
         if (!isMobile) {
           if (form.id === 'FacetSortForm' || form.id === 'FacetFiltersForm' || form.id === 'FacetSortDrawerForm') {
+            const noJsElements = document.querySelectorAll('.no-js-list');
+            noJsElements.forEach((el) => el.remove());
             forms.push(this.createSearchParams(form));
           }
         } else if (form.id === 'FacetFiltersFormMobile') {
