2402c0062bbf76e270161370650d1a7ce1bc7c0d
nathanziarczyk <nathan.ziar@gmail.com>
2022-10-27 17:13:24 +0200

    WIP Greenhouse form

---
+++ assets/greenhouse-form.js
@@ -0,0 +1,45 @@
+(function() {
+  const greenhouseSpontForm = document.querySelector("form#spontaneous_greenhouse");
+  const btnSubmit = document.querySelector("form#spontaneous_greenhouse input[type=submit]");
+  const feedbackErr = document.querySelector("form#spontaneous_greenhouse .feedback-container .feedback-error");
+  const feedbackSuccess = document.querySelector("form#spontaneous_greenhouse .feedback-container .feedback-success");
+
+  function handleFormSubmit() {
+    if(!greenhouseSpontForm) return false;
+
+    greenhouseSpontForm.addEventListener("submit", async function(e) {
+      e.preventDefault();
+      btnSubmit.disabled = true;
+      console.log({
+        "first_name": e.target[0].value,
+        "first_name": e.target[0].value,
+        "email": e.target[1].value,
+        "question_5555": e.target[2].value,
+        "data_compliance[gdpr_consent_given]": e.target[3].value
+      });
+       try {
+         await fetch("https://boards-api.greenhouse.io/v1/boards/loopearplugs/jobs/4092617101", {
+           method: "post",
+           headers: {
+             "Authorization": "Basic 8af68175223790f2fd2b9f7c04082d8b-101:",
+             "Content-Type": "application/json"
+            },
+            data: {
+              "first_name": e.target[0].value,
+              "first_name": e.target[1].value,
+              "email": e.target[2].value,
+              "question_5555": e.target[3].value,
+              "data_compliance[gdpr_consent_given]": e.target[4].value === "on"
+            }
+          })
+          feedbackSuccess.style.display = "initial";
+        } catch(error) {
+          feedbackErr.style.display = "initial";
+        }
+
+        btnSubmit.disabled = false;
+    })
+  }
+
+  handleFormSubmit();
+})()
