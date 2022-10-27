bcf6ecb282dbb30724d61f341467d033707e00e8
nathanziarczyk <nathan.ziar@gmail.com>
2022-10-27 19:24:22 +0200

    test

--- assets/greenhouse-form.js
+++ assets/greenhouse-form.js
@@ -12,31 +12,27 @@
       e.preventDefault();
       btnSubmit.disabled = true;
 
-
       const body = JSON.stringify({
-        "first_name": e.target[0].value,
-        "last_name": e.target[1].value,
-        "email": e.target[2].value,
-        "question_5555": e.target[3].value,
-        "data_compliance[gdpr_consent_given]": e.target[4].value === "on"
+        first_name: e.target[0].value,
+        last_name: e.target[1].value,
+        email: e.target[2].value,
+        question_5555: e.target[3].value
       });
 
-      const headers = new Headers;
-      headers.set('Authorization', 'Basic ' + btoa("8af68175223790f2fd2b9f7c04082d8b-101" + ":" + ""));
-      headers.set("Content-Type", "application/json")
-      headers.set("Accept", "application/json")
-
       try {
-         await fetch("https://boards-api.greenhouse.io/v1/boards/loopearplugs/jobs/4092617101", {
-            mode: "no-cors",
+         await fetch("https://greenhouseapiproxy.netlify.app/.netlify/functions/proxy", {
             method: "post",
-            headers,
+            headers: {
+              'Content-Type': 'application/json'
+            },  
             body
           })
+          feedbackErr.style.display = "none";
           feedbackSuccess.style.display = "inline-block";
           greenhouseSpontForm.reset();
         } catch(error) {
           feedbackErr.style.display = "inline-block";
+          feedbackSuccess.style.display = "none";
         }
         
         btnSubmit.disabled = false;
