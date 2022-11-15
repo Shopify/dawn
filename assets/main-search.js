1c7d992fe6d5029d853a417bd6f29956a8665f1e
Paqui Calabria <paqui.calabria@shopify.com>
2022-11-15 11:40:19 +0100

    Scroll main search to top on focus for mobile devices

--- assets/main-search.js
+++ assets/main-search.js
@@ -11,10 +11,18 @@ class MainSearch extends HTMLElement {
     if (this.allSearchInputs.length > 1) {
       this.allSearchInputs.forEach(input => input.addEventListener('input', this.onInput.bind(this)))
     }
+    this.input.addEventListener("focus", this.onInputFocus.bind(this));
   }
 
   onInput (event) {
     this.keepInSync(event.target)
+  }
+
+  onInputFocus () {
+    const isSmallScreen = window.innerWidth < 750
+    if (isSmallScreen) {
+      this.scrollIntoView({behavior: "smooth"})
+    }
   }
 
   keepInSync (target) {
