22125c6d6736254c9726e9b4c2515fecdb463ed9
nathanziarczyk <nathan.ziar@gmail.com>
2022-11-25 21:30:42 +0100

    removed max-height

--- assets/loop-rich-text-dropdown.js
+++ assets/loop-rich-text-dropdown.js
@@ -1,13 +1,13 @@
-// var acc = document.getElementsByClassName("accordion");
-// var i;
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function () {
-//     this.classList.toggle("activate");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.maxHeight) {
-//       panel.style.maxHeight = null;
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//     }
-//   });
-// }
+var acc = document.querySelector(".accordion.searchable_text");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("activate");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
