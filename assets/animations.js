9fb3ac19461910a6d5ecc1814b1c10b4a39af371
Kjell Reigstad <kjell.reigstad@shopify.com>
2022-11-28 10:37:28 -0500

    Switch to using IntersectionObserver

--- assets/animations.js
+++ assets/animations.js
@@ -1,15 +1,20 @@
 (function() {
-    window.addEventListener('scroll', animateIn);
-    animateIn();
-    function animateIn() {
-        var elements = document.querySelectorAll('.section, .banner__box, .product__info-wrapper');
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].classList.add('animate');
-            var windowHeight = window.innerHeight;
-            var scrollTop = elements[i].getBoundingClientRect().top;
-            if (scrollTop < windowHeight - 100) {
-                elements[i].classList.add('animate--active');
+    // Add animation classes.
+    function onIntersection(entries) {
+        for (const entry of entries) {
+            entry.target.classList.add('animate');
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate--active');
             }
         }
-    };
+    }
+
+    const intersectionObserver = new IntersectionObserver(onIntersection, {
+        thresholds: [1],
+    });
+    const selector = '.section, .banner__box, .product__info-wrapper';
+    const elementsToObserve = Array.from(document.querySelectorAll(selector));
+
+    // Observe intersections between the viewport and the elements.
+    elementsToObserve.forEach((element) => intersectionObserver.observe(element));
 })();
