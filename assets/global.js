d6599220dcd6e7b2c10ed01c1b4122b4849e5ebe
Sofia Matulis <sofia.matulis@shopify.com>
2022-12-06 18:33:56 -0500

    Add missing logic

--- assets/global.js
+++ assets/global.js
@@ -243,8 +243,8 @@ function validateQtyRules(cartValue, currentValue, quantityElement) {
   const step = parseInt(input.step)
 
   if ((min !== null || max !== null || step !== null) && currentValue !== null && cartValue !== null) {
-    // If the amount in cart is more than the min, thew new min is the step
-    if (parseInt(cartValue) >= min) {
+    // If the amount in cart is more than the min, thew new min is the step (only in the pdp and feat prod)
+    if (parseInt(cartValue) >= min && (Object.keys(quantityElement.dataset).length === 0)) {
       quantityElement.querySelector('input').setAttribute("min", step)
     }
     if (((parseInt(currentValue) + parseInt(cartValue))) <= min) {
