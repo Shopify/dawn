fc85e20dc41cfb2ebeb5337823b5f8e60742c0e3
Kai <KaichenWang@users.noreply.github.com>
2023-01-13 16:34:26 -0500

    Add quantity rules and cart awareness (#2203)
    * Qty rules demo
    
    * Prototype qty rules
    
    * Check default
    
    * Add qty for collection
    
    * Remove unused var
    
    * Add comment
    
    * Remove unused element
    
    * Add update qty api rules
    ;
    
    * Clean up class
    
    * Add on change
    
    * Designs
    
    * Remove unused code
    
    * prototype
    
    * Add logic for qty
    
    * Update max
    
    * feat product
    
    * Add logic
    
    * Add missing logic
    
    * Clean up
    
    * Cart drawer
    
    * Remove console
    
    * Fix logic
    
    * Fix section rendering api
    
    * Updae variant logic
    
    * Updae variant logic
    
    * Clean up code
    
    * Remove hardcoded error
    
    * Refactor approach
    
    * Update approach to use evt listeners
    
    * Move logic
    
    * Update qty when element is removed from cart
    
    * Add remove event listener
    
    * Refactor and add pubsub
    
    * Clean up
    
    * Clean up code
    
    * Clean up
    
    * Remove unused code
    
    * Implement qty rules
    
    * Remove space
    
    * Update logic
    
    * Refactor pubsub
    
    * Clean up
    
    * Remove modules for now and fix cart drawer
    
    * Clean up
    
    * Clean up feedback
    
    * Spacing
    
    * Clean up
    
    * Clean up cart
    
    * Create modal for qty rules
    
    * Update defaults
    
    * Comment out code for now
    
    * Add class list
    
    * Clean up
    
    * Clean up
    
    * Fix feat prod
    
    * Fix position
    
    * Move quantity update
    
    * Add buy button
    
    * Add missing class
    
    * Fix amount
    
    * Ensure values update right away;
    
    * Clean up code
    
    * Fix show options
    
    * Fix closing tag
    
    * Fix class name
    
    * requested changes
    
    minor cleanup
    
    * Remove redundant check for tagName in callback
    
    * Prettier - Add semicolon to end of line
    
    * Clean up logic. Use toggleclass
    
    * Reuse cart_qty variable. Add todos
    
    * Update data attribute naming
    
    * Update data attribute name
    
    * Remove redundant role="presentation" from spinner svg
    
    * Remove redundant data-min attribute
    
    * Reuse cart_qty variable to reduce verbosity
    
    * Add strings to english locales file
    
    * Apply prettier
    
    * Store pubsub event names in constant. Separate cart updates triggered by cart and product into 2 different pubsub event names
    
    * Surface server side messages for line items when cart returns error on change
    
    * Set quantity rule font size to 1.2rem (to override caption mobile font size)
    
    * Add pubsub event source to differentiate events, instead of using different event names
    
    * Move cart quantity inside product info next to input label
    
    * Add new line at end of file
    
    * Address feedback. Remove comments and fix typos
    
    * Fix after rebase
    
    * Update 21 translation files
    
    * Update 9 translation files
    
    * Update "Multiples of..." to "Increments of...". Keeping the key the same for now
    
    * Move spinner to cover cart quantity when item is added to cart
    
    * Fix inconsistent cart error messages when inventory limits causes server-side quantity clamping
    
    * Update logic for setting limit values and validation on quantity inputs
    
    * Move "increments of..." copy before min and max
    
    * Update quantity_limit on the ProductVariantDrop to quantity_rule
    
    * Avoid showing spinner for cart quantity if add-to-cart results in error
    
    * Use margin CSS shorthand
    
    * Update 20 translation files
    
    * Update 8 translation files
    
    Co-authored-by: Sofia Matulis <sofia.matulis@shopify.com>
    Co-authored-by: translation-platform[bot] <34770790+translation-platform[bot]@users.noreply.github.com>

--- assets/global.js
+++ assets/global.js
@@ -146,11 +146,29 @@ class QuantityInput extends HTMLElement {
   constructor() {
     super();
     this.input = this.querySelector('input');
-    this.changeEvent = new Event('change', { bubbles: true })
+    this.changeEvent = new Event('change', { bubbles: true });
 
+    this.input.addEventListener('change', this.onInputChange.bind(this));
     this.querySelectorAll('button').forEach(
       (button) => button.addEventListener('click', this.onButtonClick.bind(this))
     );
+  }
+
+  quantityUpdateUnsubscriber = undefined;
+
+  connectedCallback() {
+    this.validateQtyRules();
+    this.quantityUpdateUnsubscriber = subscribe(PUB_SUB_EVENTS.quantityUpdate, this.validateQtyRules.bind(this));
+  }
+
+  disconnectedCallback() {
+    if (this.quantityUpdateUnsubscriber) {
+      this.quantityUpdateUnsubscriber();
+    }
+  }
+
+  onInputChange(event) {
+    this.validateQtyRules();
   }
 
   onButtonClick(event) {
@@ -159,6 +177,20 @@ class QuantityInput extends HTMLElement {
 
     event.target.name === 'plus' ? this.input.stepUp() : this.input.stepDown();
     if (previousValue !== this.input.value) this.input.dispatchEvent(this.changeEvent);
+  }
+
+  validateQtyRules() {
+    const value = parseInt(this.input.value);
+    if (this.input.min) {
+      const min = parseInt(this.input.min);
+      const buttonMinus = this.querySelector(".quantity__button[name='minus']");
+      buttonMinus.classList.toggle('disabled', value <= min);
+    }
+    if (this.input.max) {
+      const max = parseInt(this.input.max);
+      const buttonPlus = this.querySelector(".quantity__button[name='plus']");
+      buttonPlus.classList.toggle('disabled', value >= max);
+    } 
   }
 }
 
