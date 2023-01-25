5f29d76ec4474ba9bf9eb7d729c361808ab9cdd5
Stu Freen <stu.freen@shopify.com>
2023-01-25 14:06:28 -0500

    Add basic intersection observer

---
+++ assets/scroll-trigger.js
@@ -0,0 +1,31 @@
+const SCROLL_TRIGGER_CLASSNAME = "scroll-trigger";
+const IN_VIEW_CLASSNAME = "scrolled-into-view";
+
+const OPTIONS = {
+  threshold: 0.5,
+};
+
+function onIntersection(entries, observer) {
+  for (const entry of entries) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add(IN_VIEW_CLASSNAME);
+      observer.unobserve(entry.target);
+    }
+  }
+}
+
+function initializeScrollTrigger() {
+  const scrollTriggerElements = Array.from(
+    document.getElementsByClassName(SCROLL_TRIGGER_CLASSNAME)
+  );
+
+  if (scrollTriggerElements.length === 0) {
+    return;
+  }
+
+  const observer = new IntersectionObserver(onIntersection, OPTIONS);
+
+  scrollTriggerElements.forEach((element) => observer.observe(element));
+}
+
+window.addEventListener("load", initializeScrollTrigger);
