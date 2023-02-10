6fb2a7d63fb9f9a12c39d2b6d4363a47519aebb2
nathanziarczyk <nathan.ziar@gmail.com>
2023-02-10 16:24:00 +0100

    exclude spontaneous

--- assets/greenhouse-form.js
+++ assets/greenhouse-form.js
@@ -1,16 +1,16 @@
-(function() {
+(function () {
   const greenhouseSpontForm = document.querySelector("form#spontaneous_greenhouse--form");
   const btnSubmit = document.querySelector("form#spontaneous_greenhouse--form input[type=submit]");
   const feedbackErr = document.querySelector("form#spontaneous_greenhouse--form .feedback-container .feedback-error");
   const feedbackSuccess = document.querySelector("form#spontaneous_greenhouse--form .feedback-container .feedback-success");
 
   function handleFormSubmit() {
-    if(!greenhouseSpontForm) return false;
+    if (!greenhouseSpontForm) return false;
 
-    
-    greenhouseSpontForm.addEventListener("submit", async function(e) {
+
+    greenhouseSpontForm.addEventListener("submit", async function (e) {
       e.preventDefault();
-      btnSubmit.disabled = true;
+      // btnSubmit.disabled = true;
 
       const body = JSON.stringify({
         first_name: e.target[0].value,
@@ -20,23 +20,23 @@
       });
 
       try {
-        await fetch("https://greenhouseapiproxy.loopearplugs.workers.dev/", {
-        method: "post",
-            headers: {
-              'Content-Type': 'application/json'
-            },  
-            mode: "no-cors",
-            body
-          })
-          feedbackErr.style.display = "none";
-          feedbackSuccess.style.display = "inline-block";
-          greenhouseSpontForm.reset();
-        } catch(error) {
-          feedbackErr.style.display = "inline-block";
-          feedbackSuccess.style.display = "none";
-        }
-        
-        btnSubmit.disabled = false;
+        await fetch("http://127.0.0.1:8787/", {
+          method: "post",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          mode: "no-cors",
+          body
+        })
+        feedbackErr.style.display = "none";
+        feedbackSuccess.style.display = "inline-block";
+        // greenhouseSpontForm.reset();
+      } catch (error) {
+        feedbackErr.style.display = "inline-block";
+        feedbackSuccess.style.display = "none";
+      }
+
+      btnSubmit.disabled = false;
     })
   }
 
