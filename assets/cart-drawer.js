71830c1289da3384166f1387c82bbcc1cc0363bf
Seymur Rahimov <noiseymur@hotmail.com>
2023-02-27 13:05:33 +0400

    Some change

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -2,21 +2,29 @@ class CartDrawer extends HTMLElement {
   constructor() {
     super();
 
-    this.addEventListener('keyup', (evt) => evt.code === 'Escape' && this.close());
-    this.querySelector('#CartDrawer-Overlay').addEventListener('click', this.close.bind(this));
+    console.log("Some change");
+
+    this.addEventListener(
+      "keyup",
+      (evt) => evt.code === "Escape" && this.close()
+    );
+    this.querySelector("#CartDrawer-Overlay").addEventListener(
+      "click",
+      this.close.bind(this)
+    );
     this.setHeaderCartIconAccessibility();
   }
 
   setHeaderCartIconAccessibility() {
-    const cartLink = document.querySelector('#cart-icon-bubble');
-    cartLink.setAttribute('role', 'button');
-    cartLink.setAttribute('aria-haspopup', 'dialog');
-    cartLink.addEventListener('click', (event) => {
+    const cartLink = document.querySelector("#cart-icon-bubble");
+    cartLink.setAttribute("role", "button");
+    cartLink.setAttribute("aria-haspopup", "dialog");
+    cartLink.addEventListener("click", (event) => {
       event.preventDefault();
-      this.open(cartLink)
+      this.open(cartLink);
     });
-    cartLink.addEventListener('keydown', (event) => {
-      if (event.code.toUpperCase() === 'SPACE') {
+    cartLink.addEventListener("keydown", (event) => {
+      if (event.code.toUpperCase() === "SPACE") {
         event.preventDefault();
         this.open(cartLink);
       }
@@ -26,76 +34,101 @@ class CartDrawer extends HTMLElement {
   open(triggeredBy) {
     if (triggeredBy) this.setActiveElement(triggeredBy);
     const cartDrawerNote = this.querySelector('[id^="Details-"] summary');
-    if (cartDrawerNote && !cartDrawerNote.hasAttribute('role')) this.setSummaryAccessibility(cartDrawerNote);
+    if (cartDrawerNote && !cartDrawerNote.hasAttribute("role"))
+      this.setSummaryAccessibility(cartDrawerNote);
     // here the animation doesn't seem to always get triggered. A timeout seem to help
-    setTimeout(() => {this.classList.add('animate', 'active')});
+    setTimeout(() => {
+      this.classList.add("animate", "active");
+    });
 
-    this.addEventListener('transitionend', () => {
-      const containerToTrapFocusOn = this.classList.contains('is-empty') ? this.querySelector('.drawer__inner-empty') : document.getElementById('CartDrawer');
-      const focusElement = this.querySelector('.drawer__inner') || this.querySelector('.drawer__close');
-      trapFocus(containerToTrapFocusOn, focusElement);
-    }, { once: true });
+    this.addEventListener(
+      "transitionend",
+      () => {
+        const containerToTrapFocusOn = this.classList.contains("is-empty")
+          ? this.querySelector(".drawer__inner-empty")
+          : document.getElementById("CartDrawer");
+        const focusElement =
+          this.querySelector(".drawer__inner") ||
+          this.querySelector(".drawer__close");
+        trapFocus(containerToTrapFocusOn, focusElement);
+      },
+      { once: true }
+    );
 
-    document.body.classList.add('overflow-hidden');
+    document.body.classList.add("overflow-hidden");
   }
 
   close() {
-    this.classList.remove('active');
+    this.classList.remove("active");
     removeTrapFocus(this.activeElement);
-    document.body.classList.remove('overflow-hidden');
+    document.body.classList.remove("overflow-hidden");
   }
 
   setSummaryAccessibility(cartDrawerNote) {
-    cartDrawerNote.setAttribute('role', 'button');
-    cartDrawerNote.setAttribute('aria-expanded', 'false');
+    cartDrawerNote.setAttribute("role", "button");
+    cartDrawerNote.setAttribute("aria-expanded", "false");
 
-    if(cartDrawerNote.nextElementSibling.getAttribute('id')) {
-      cartDrawerNote.setAttribute('aria-controls', cartDrawerNote.nextElementSibling.id);
+    if (cartDrawerNote.nextElementSibling.getAttribute("id")) {
+      cartDrawerNote.setAttribute(
+        "aria-controls",
+        cartDrawerNote.nextElementSibling.id
+      );
     }
 
-    cartDrawerNote.addEventListener('click', (event) => {
-      event.currentTarget.setAttribute('aria-expanded', !event.currentTarget.closest('details').hasAttribute('open'));
+    cartDrawerNote.addEventListener("click", (event) => {
+      event.currentTarget.setAttribute(
+        "aria-expanded",
+        !event.currentTarget.closest("details").hasAttribute("open")
+      );
     });
 
-    cartDrawerNote.parentElement.addEventListener('keyup', onKeyUpEscape);
+    cartDrawerNote.parentElement.addEventListener("keyup", onKeyUpEscape);
   }
 
   renderContents(parsedState) {
-    this.querySelector('.drawer__inner').classList.contains('is-empty') && this.querySelector('.drawer__inner').classList.remove('is-empty');
+    this.querySelector(".drawer__inner").classList.contains("is-empty") &&
+      this.querySelector(".drawer__inner").classList.remove("is-empty");
     this.productId = parsedState.id;
-    this.getSectionsToRender().forEach((section => {
-      const sectionElement = section.selector ? document.querySelector(section.selector) : document.getElementById(section.id);
-      sectionElement.innerHTML =
-          this.getSectionInnerHTML(parsedState.sections[section.id], section.selector);
-    }));
+    this.getSectionsToRender().forEach((section) => {
+      const sectionElement = section.selector
+        ? document.querySelector(section.selector)
+        : document.getElementById(section.id);
+      sectionElement.innerHTML = this.getSectionInnerHTML(
+        parsedState.sections[section.id],
+        section.selector
+      );
+    });
 
     setTimeout(() => {
-      this.querySelector('#CartDrawer-Overlay').addEventListener('click', this.close.bind(this));
+      this.querySelector("#CartDrawer-Overlay").addEventListener(
+        "click",
+        this.close.bind(this)
+      );
       this.open();
     });
   }
 
-  getSectionInnerHTML(html, selector = '.shopify-section') {
+  getSectionInnerHTML(html, selector = ".shopify-section") {
     return new DOMParser()
-      .parseFromString(html, 'text/html')
+      .parseFromString(html, "text/html")
       .querySelector(selector).innerHTML;
   }
 
   getSectionsToRender() {
     return [
       {
-        id: 'cart-drawer',
-        selector: '#CartDrawer'
+        id: "cart-drawer",
+        selector: "#CartDrawer",
       },
       {
-        id: 'cart-icon-bubble'
-      }
+        id: "cart-icon-bubble",
+      },
     ];
   }
 
-  getSectionDOM(html, selector = '.shopify-section') {
+  getSectionDOM(html, selector = ".shopify-section") {
     return new DOMParser()
-      .parseFromString(html, 'text/html')
+      .parseFromString(html, "text/html")
       .querySelector(selector);
   }
 
@@ -104,23 +137,23 @@ class CartDrawer extends HTMLElement {
   }
 }
 
-customElements.define('cart-drawer', CartDrawer);
+customElements.define("cart-drawer", CartDrawer);
 
 class CartDrawerItems extends CartItems {
   getSectionsToRender() {
     return [
       {
-        id: 'CartDrawer',
-        section: 'cart-drawer',
-        selector: '.drawer__inner'
+        id: "CartDrawer",
+        section: "cart-drawer",
+        selector: ".drawer__inner",
       },
       {
-        id: 'cart-icon-bubble',
-        section: 'cart-icon-bubble',
-        selector: '.shopify-section'
-      }
+        id: "cart-icon-bubble",
+        section: "cart-icon-bubble",
+        selector: ".shopify-section",
+      },
     ];
   }
 }
 
-customElements.define('cart-drawer-items', CartDrawerItems);
+customElements.define("cart-drawer-items", CartDrawerItems);
