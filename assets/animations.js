3c691c21633c5e7499068ba0d7dc77bf8f381d5c
Ludo Segura <ludo.segura@shopify.com>
2023-04-06 15:35:15 -0400

    Prevent animation on elements within the viewport on page load

--- assets/animations.js
+++ assets/animations.js
@@ -13,9 +13,31 @@ function onIntersection(elements, observer) {
   });
 }
 
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  console.log(rect);
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
 function initializeScrollAnimationTrigger(rootEl = document) {
   const animationTriggerElements = Array.from(rootEl.getElementsByClassName(SCROLL_ANIMATION_TRIGGER_CLASSNAME));
   if (animationTriggerElements.length === 0) return;
+
+  console.log(isInViewport(animationTriggerElements[0]));
+
+  animationTriggerElements.forEach((element) => {
+    if (isInViewport(element)) {
+      element.classList.remove(SCROLL_ANIMATION_ACTIVE_CLASSNAME);
+      element.classList.remove(SCROLL_ANIMATION_TRIGGER_CLASSNAME);
+      element.classList.remove('animate--slide-in');
+      element.style = `--animation-order: 0;`;
+    }
+  });
 
   const observer = new IntersectionObserver(onIntersection, {
     threshold: 0.1,
