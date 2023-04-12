a6697d20fe39c0dc1eebe822dab6901f2fd6e5b1
David Altenburg <david@gensym.org>
2023-04-12 16:19:24 -0700

    convert xcp modal to not require jquery

--- assets/xcp-modal.js
+++ assets/xcp-modal.js
@@ -1,33 +1,33 @@
-$(function() {
-  var xcpModalTriggers = $('.xcp-modal');
+document.addEventListener('DOMContentLoaded', function() {
+  var xcpModalTriggers = document.querySelectorAll('.xcp-modal');
   if (!xcpModalTriggers.length) {
     return;
   }
 
   function init(trigger) {
-    var modalTargetClass = '.' + trigger.data('modalTarget');
-    var closedClass      = trigger.data('modalTargetClosed');
-    var closerClass      = '.' + trigger.data('modalCloser');
+    var modalTargetClass = '.' + trigger.getAttribute('data-modalTarget');
+    var closedClass      = trigger.getAttribute('data-modalTargetClosed');
+    var closerClass      = '.' + trigger.getAttribute('data-modalCloser');
 
-    var modalTarget = $(modalTargetClass);
-    var closer      = $(closerClass);
+    var modalTarget = document.querySelector(modalTargetClass);
+    var closer      = document.querySelector(closerClass);
 
-    trigger.on('click', function() {
-      modalTarget.removeClass(closedClass);
+    trigger.addEventListener('click', function() {
+      modalTarget.classList.remove(closedClass);
     });
 
-    closer.on('click', function() {
-      modalTarget.addClass(closedClass);
+    closer.addEventListener('click', function() {
+      modalTarget.classList.add(closedClass);
     });
 
-    modalTarget.on('click', function(event) {
+    modalTarget.addEventListener('click', function(event) {
       if (event.target === event.currentTarget) {
-        modalTarget.addClass(closedClass);
+        modalTarget.classList.add(closedClass);
       }
     });
   }
 
-  xcpModalTriggers.each(function(_, trigger) {
-    init($(trigger));
+  xcpModalTriggers.forEach(function(trigger) {
+    init(trigger);
   });
 });
