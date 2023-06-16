887036a062217a2315966b7c1128a775dcb86c88
Sofia Matulis <sofia.matulis@shopify.com>
2023-06-16 11:21:11 -0400

    Feedback

--- assets/quantity-popover.js
+++ assets/quantity-popover.js
@@ -1,27 +1,33 @@
 class QuantityPopover extends HTMLElement {
   constructor() {
     super();
+    this.screenSize = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     this.infoButton = this.querySelector('.variant-item__quantity-info.medium-hide');
     this.popoverInfo = this.querySelector('.quantity-popover__info');
     this.closeButton = this.querySelector('.button-close');
+    this.variantInfo = this.querySelector('.variant-item__quantity-wrap');
 
     this.infoButton.addEventListener('click', this.togglePopover.bind(this));
     this.closeButton.addEventListener('click', this.closePopover.bind(this));
     this.infoButton.addEventListener('focusout', this.closePopover.bind(this));
     this.popoverInfo.addEventListener('focusout', this.closePopover.bind(this));
-    this.addEventListener('mouseenter', this.toggleAccessibility.bind(this));
-    this.addEventListener('mouseleave', this.toggleAccessibility.bind(this));
+
+    if (this.screenSize > 749) {
+      this.addEventListener('mouseenter', this.toggleAccessibility.bind(this));
+      this.addEventListener('mouseleave', this.closePopover.bind(this));
+    }
   }
 
   togglePopover(event) {
     event.preventDefault();
+    const isExpanded = this.infoButton.getAttribute('aria-expanded') === true;
 
     this.infoButton.setAttribute(
       'aria-expanded',
-      (this.infoButton.getAttribute('aria-expanded') === 'false').toString()
+      !isExpanded
     );
 
-    if (this.infoButton.getAttribute('aria-expanded') === 'true') {
+    if (isExpanded) {
       this.closeButton.focus();
     }
     this.toggleAccessibility(event);
@@ -34,8 +40,8 @@ class QuantityPopover extends HTMLElement {
 
   closePopover(event) {
     event.preventDefault();
-    const isChild = this.popoverInfo.contains(event.relatedTarget) || this.querySelector('.variant-item__info').contains(event.relatedTarget);
-    console.log(isChild, event.relatedTarget, 'ehhehehe')
+    const isChild = this.popoverInfo.contains(event.relatedTarget) || this.variantInfo.contains(event.relatedTarget);
+
     if (!event.relatedTarget || !isChild) {
       this.popoverInfo.setAttribute('hidden', true);
     }
