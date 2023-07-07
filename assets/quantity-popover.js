f2bc437f2e628a6f23f1f5475d734997c6d7550b
Sofia Matulis <sofiamatulis@users.noreply.github.com>
2023-07-07 09:41:49 -0400

    Variant List (#2627)

--- assets/quantity-popover.js
+++ assets/quantity-popover.js
@@ -4,10 +4,9 @@ if (!customElements.get('quantity-popover')) {
     class QuantityPopover extends HTMLElement {
       constructor() {
         super();
-        this.mql = window.matchMedia('(min-width: 990px)');
-        this.mqlTablet = window.matchMedia('(min-width: 750px)');
-        this.infoButtonDesktop = this.querySelector('.quantity-popover__info-button--icon-only');
-        this.infoButtonMobile = this.querySelector('.quantity-popover__info-button--icon-with-label');
+        this.mql = window.matchMedia('(min-width: 750px)');
+        this.infoButtonDesktop = this.querySelector('.quantity-popover__info-button.small-hide');
+        this.infoButtonMobile = this.querySelector('.quantity-popover__info-button.medium-hide');
         this.popoverInfo = this.querySelector('.quantity-popover__info');
         this.closeButton = this.querySelector('.button-close');
         this.variantInfo = this.querySelector('.quantity-popover-container');
@@ -16,20 +15,18 @@ if (!customElements.get('quantity-popover')) {
           this.closeButton.addEventListener('click', this.closePopover.bind(this));
         }
 
-        if (this.popoverInfo && this.infoButtonDesktop && this.mql.matches) {
+        if (this.popoverInfo) {
           this.popoverInfo.addEventListener('mouseenter', this.closePopover.bind(this));
         }
 
-        if (this.infoButtonDesktop) {
+        if (this.infoButtonDesktop && this.infoButtonMobile) {
           this.infoButtonDesktop.addEventListener('click', this.togglePopover.bind(this));
-          this.infoButtonDesktop.addEventListener('focusout', this.closePopover.bind(this));
-        }
-
-        if (this.infoButtonMobile) {
           this.infoButtonMobile.addEventListener('click', this.togglePopover.bind(this));
+          this.infoButtonDesktop.addEventListener('focusout', this.closePopover.bind(this));
           this.infoButtonMobile.addEventListener('focusout', this.closePopover.bind(this));
         }
-        if (this.infoButtonDesktop && this.mqlTablet.matches) {
+
+        if (this.infoButtonDesktop && this.mql.matches) {
           this.variantInfo.addEventListener('mouseenter', this.togglePopover.bind(this));
           this.variantInfo.addEventListener('mouseleave', this.closePopover.bind(this));
         }
@@ -38,7 +35,7 @@ if (!customElements.get('quantity-popover')) {
       togglePopover(event) {
         event.preventDefault();
 
-        const button = this.infoButtonDesktop && this.mql.matches ? this.infoButtonDesktop : this.infoButtonMobile;
+        const button = this.mql.matches ? this.infoButtonDesktop : this.infoButtonMobile;
         const isExpanded = button.getAttribute('aria-expanded') === 'true';
 
         button.setAttribute('aria-expanded', !isExpanded);
@@ -58,7 +55,7 @@ if (!customElements.get('quantity-popover')) {
         event.preventDefault();
         const isChild = this.variantInfo.contains(event.relatedTarget);
 
-        const button = this.infoButtonDesktop && this.mql.matches ? this.infoButtonDesktop : this.infoButtonMobile;
+        const button = this.mql.matches ? this.infoButtonDesktop : this.infoButtonMobile;
 
         if (!event.relatedTarget || !isChild) {
           button.setAttribute('aria-expanded', 'false');
