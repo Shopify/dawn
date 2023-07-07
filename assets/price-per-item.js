cea3715cb8b82f7c38dacb3d4bc492631a248ceb
Sofia Matulis <sofiamatulis@users.noreply.github.com>
2023-07-07 09:42:53 -0400

    Volume Pricing for PDP (#2711)

--- assets/price-per-item.js
+++ assets/price-per-item.js
@@ -4,16 +4,15 @@ if (!customElements.get('price-per-item')) {
     class PricePerItem extends HTMLElement {
       constructor() {
         super();
+        this.input = document.getElementById(`Quantity-${this.dataset.sectionId}`);
         this.variantId = this.dataset.variantId;
-        this.input = document.getElementById(`Quantity-${this.dataset.sectionId || this.dataset.variantId}`);
-        if (this.input) {
-          this.input.addEventListener('change', this.onInputChange.bind(this));
-        }
+        this.input.addEventListener('change', this.onInputChange.bind(this));
 
         this.getVolumePricingArray();
       }
 
       updatePricePerItemUnsubscriber = undefined;
+      cartUpdateStartedUnsubscriber = undefined;
       variantIdChangedUnsubscriber = undefined;
 
       connectedCallback() {
@@ -22,24 +21,27 @@ if (!customElements.get('price-per-item')) {
           this.variantId = event.data.variant.id.toString();
           this.getVolumePricingArray();
         });
+        this.cartUpdateStartedUnsubscriber = subscribe(PUB_SUB_EVENTS.cartUpdateStarted, (event) => {
+          if (event.variantId !== this.variantId) return;
+          this.querySelector('.loading-overlay').classList.remove('hidden');
+        });
 
         this.updatePricePerItemUnsubscriber = subscribe(PUB_SUB_EVENTS.cartUpdate, (response) => {
-          if (!response.cartData) return;
-
+          this.querySelector('.loading-overlay').classList.add('hidden');
           // Item was added to cart via product page
           if (response.cartData['variant_id'] !== undefined) {
-            if (response.productVariantId === this.variantId) this.updatePricePerItem(response.cartData.quantity);
-            // Qty was updated in cart
+            this.updatePricePerItem(response.cartData.quantity);
+          // Qty was updated in cart
           } else if (response.cartData.item_count !== 0) {
             const isVariant = response.cartData.items.find((item) => item.variant_id.toString() === this.variantId);
-            if (isVariant && isVariant.id.toString() === this.variantId) {
+            if (isVariant) {
               // The variant is still in cart
               this.updatePricePerItem(isVariant.quantity);
             } else {
               // The variant was removed from cart, qty is 0
               this.updatePricePerItem(0);
             }
-            // All items were removed from cart
+          // All items were removed from cart
           } else {
             this.updatePricePerItem(0);
           }
@@ -49,6 +51,9 @@ if (!customElements.get('price-per-item')) {
       disconnectedCallback() {
         if (this.updatePricePerItemUnsubscriber) {
           this.updatePricePerItemUnsubscriber();
+        }
+        if (this.cartUpdateStartedUnsubscriber) {
+          this.cartUpdateStartedUnsubscriber();
         }
         if (this.variantIdChangedUnsubscriber) {
           this.variantIdChangedUnsubscriber();
@@ -60,23 +65,16 @@ if (!customElements.get('price-per-item')) {
       }
 
       updatePricePerItem(updatedCartQuantity) {
-        if (this.input) {
-          this.enteredQty = parseInt(this.input.value);
-          this.step = parseInt(this.input.step)
-        }
+        const enteredQty = parseInt(this.input.value);
 
         // updatedCartQuantity is undefined when qty is updated on product page. We need to sum entered qty and current qty in cart.
         // updatedCartQuantity is not undefined when qty is updated in cart. We need to sum qty in cart and min qty for product.
-        this.currentQtyForVolumePricing = updatedCartQuantity === undefined ? this.getCartQuantity(updatedCartQuantity) + this.enteredQty : this.getCartQuantity(updatedCartQuantity) + parseInt(this.step);
-
-        if (this.classList.contains('variant-item__price-per-item')) {
-          this.currentQtyForVolumePricing = this.getCartQuantity(updatedCartQuantity);
-        }
+        this.currentQtyForVolumePricing = updatedCartQuantity === undefined ? this.getCartQuantity(updatedCartQuantity) + enteredQty : this.getCartQuantity(updatedCartQuantity) + parseInt(this.input.min);
 
         for (let pair of this.qtyPricePairs) {
           if (this.currentQtyForVolumePricing >= pair[0]) {
-            const pricePerItemCurrent = document.querySelector(`price-per-item[id^="Price-Per-Item-${this.dataset.sectionId || this.dataset.variantId}"] .price-per-item span`);
-            this.classList.contains('variant-item__price-per-item') ? pricePerItemCurrent.innerHTML = window.quickOrderListStrings.each.replace('[money]', pair[1]) : pricePerItemCurrent.innerHTML = pair[1];
+            const pricePerItemCurrent = document.querySelector(`price-per-item[id^="Price-Per-Item-${this.dataset.sectionId}"] .price-per-item`);
+            pricePerItemCurrent.innerHTML = pair[1];
             break;
           }
         }
@@ -87,16 +85,14 @@ if (!customElements.get('price-per-item')) {
       }
 
       getVolumePricingArray() {
-        const volumePricing = document.getElementById(`Volume-${this.dataset.sectionId || this.dataset.variantId}`);
+        const volumePricing = document.getElementById(`Volume-${this.dataset.sectionId}`);
         this.qtyPricePairs = [];
 
-        if (volumePricing) {
-          volumePricing.querySelectorAll('li').forEach(li => {
-            const qty = parseInt(li.querySelector('span:first-child').textContent);
-            const price = (li.querySelector('span:not(:first-child):last-child').dataset.text);
-            this.qtyPricePairs.push([qty, price]);
-          });
-        }
+        volumePricing.querySelectorAll('li').forEach(li => {
+          const qty = parseInt(li.querySelector('span:first-child').textContent);
+          const price = (li.querySelector('span:not(:first-child):last-child').dataset.text);
+          this.qtyPricePairs.push([qty, price]);
+        });
 
         this.qtyPricePairs.reverse();
       }
