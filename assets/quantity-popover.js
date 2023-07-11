27ced7dc621b810901c351164cc3fc31b8e79a07
Kai <kai.wang@shopify.com>
2023-07-11 16:29:23 -0400

    Update media queries for volume pricing popover

--- assets/quantity-popover.js
+++ assets/quantity-popover.js
@@ -4,7 +4,7 @@ if (!customElements.get('quantity-popover')) {
     class QuantityPopover extends HTMLElement {
       constructor() {
         super();
-        this.mql = window.matchMedia('(min-width: 750px)');
+        this.mql = window.matchMedia('(min-width: 990px)');
         this.infoButtonDesktop = this.querySelector('.quantity-popover__info-button--icon-only');
         this.infoButtonMobile = this.querySelector('.quantity-popover__info-button--icon-with-label');
         this.popoverInfo = this.querySelector('.quantity-popover__info');
