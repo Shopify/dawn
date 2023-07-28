2276efe4a057dbddd40fb9f2a0c648748b3375d6
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2023-07-28 16:17:13 -0300

    wip: swipe carousel on mobile

--- assets/easel-text-review-carousel.js
+++ assets/easel-text-review-carousel.js
@@ -44,23 +44,69 @@ document.addEventListener('DOMContentLoaded', function() {
       const index = target.dataset.index
       const carouselItems = Array.from(document.querySelectorAll('.easel-text-reviews__carousel-item:not(.template)'))
       const carouselIndicators = Array.from(document.querySelectorAll('.easel-text-reviews__carousel-indicator:not(.template)'))
-      const changeActive = (collection) => {
+      const changeActive = (targetIndex, collection) => {
         for (let i = 0; i < collection.length; i++) {
           const item = collection[i]
           item.classList.remove('active')
 
-          if (i === parseInt(index)) {
+          if (i === parseInt(targetIndex)) {
             item.classList.add('active')
           }
         }
 
       }
 
-      changeActive(carouselItems)
-      changeActive(carouselIndicators)
+      changeActive(index, carouselItems)
+      changeActive(index, carouselIndicators)
     })
 
     carouselElem.appendChild(newCarouselItem)
     carouselIndicators.appendChild(newCarouselIndicator)
   }
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+  const carouselMobile = document.querySelector('')
+  const mobileIndicators = Array.from(document.querySelectorAll('.easel-text-reviews__carousel-indicator-mobile')) 
+
+  var touchsurface = document.getElementById('touchsurface'),
+  startX,
+  startY,
+  dist,
+  threshold = 150, //required min distance traveled to be considered swipe
+  allowedTime = 200, // maximum time allowed to travel that distance
+  elapsedTime,
+  startTime
+
+  function handleswipe(isrightswipe){
+    if (isrightswipe)
+    touchsurface.innerHTML = 'Congrats, you\'ve made a <span style="color:red">right swipe!</span>'
+    else{
+      touchsurface.innerHTML = 'Condition for right swipe not met yet'
+    }
+  }
+
+  touchsurface.addEventListener('touchstart', function(e){
+    touchsurface.innerHTML = ''
+    var touchobj = e.changedTouches[0]
+    dist = 0
+    startX = touchobj.pageX
+    startY = touchobj.pageY
+    startTime = new Date().getTime() // record time when finger first makes contact with surface
+    e.preventDefault()
+  }, false)
+
+  touchsurface.addEventListener('touchmove', function(e){
+    e.preventDefault() // prevent scrolling when inside DIV
+  }, false)
+
+  touchsurface.addEventListener('touchend', function(e){
+    var touchobj = e.changedTouches[0]
+    dist = touchobj.pageX - startX // get total dist traveled by finger while in contact with surface
+    elapsedTime = new Date().getTime() - startTime // get time elapsed
+    // check that elapsed time is within specified, horizontal dist traveled >= threshold, and vertical dist traveled <= 100
+    var swiperightBol = (elapsedTime <= allowedTime && dist >= threshold && Math.abs(touchobj.pageY - startY) <= 100)
+    handleswipe(swiperightBol)
+    e.preventDefault()
+  }, false)
 })
