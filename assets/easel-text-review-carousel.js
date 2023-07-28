b07c99f503831e52c47773888c8caed27a2dc732
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2023-07-28 14:31:48 -0300

    wip: carousel script

--- assets/easel-text-review-carousel.js
+++ assets/easel-text-review-carousel.js
@@ -21,15 +21,44 @@ document.addEventListener('DOMContentLoaded', function() {
     newCarouselItem = carouselItemTemplate.cloneNode()
     newCarouselItem.classList.remove('hidden')
     newCarouselItem.classList.remove('template')
+    newCarouselItem.dataset['index'] = i
     newCarouselIndicator = carouselIndicatorTemplate.cloneNode(true)
     newCarouselIndicator.classList.remove('hidden')
     newCarouselIndicator.classList.remove('template')
+    newCarouselIndicator.dataset['index'] = i
+
     slice = itemsSlice(i)
     for(const item of slice) {
       item.classList.remove('hidden')
       item.classList.remove('template')
       newCarouselItem.appendChild(item)
     }
+
+    if (i === 0) {
+      newCarouselItem.classList.add('active')
+      newCarouselIndicator.classList.add('active')
+    }
+
+    newCarouselIndicator.addEventListener('click', function(e) {
+      const target = e.target
+      const index = target.dataset.index
+      const carouselItems = Array.from(document.querySelectorAll('.easel-text-reviews__carousel-item:not(.template)'))
+      const carouselIndicators = Array.from(document.querySelectorAll('.easel-text-reviews__carousel-indicator:not(.template)'))
+      const changeActive = (collection) => {
+        for (let i = 0; i < collection.length; i++) {
+          const item = collection[i]
+          item.classList.remove('active')
+
+          if (i === parseInt(index)) {
+            item.classList.add('active')
+          }
+        }
+
+      }
+
+      changeActive(carouselItems)
+      changeActive(carouselIndicators)
+    })
 
     carouselElem.appendChild(newCarouselItem)
     carouselIndicators.appendChild(newCarouselIndicator)
