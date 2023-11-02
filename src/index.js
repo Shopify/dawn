94d01d9fc30497b80a4ada720eac2dff9cfb5fde
riku takenaka <riijon@gmail.com>
2023-11-02 17:35:50 +0900

    [update] 憲靖さんFBとデザイン調整

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import './css/input.css'
 import './sass/app.scss'
-import {register} from 'swiper/element/bundle';
+import { register } from 'swiper/element';
+import { Navigation, Pagination, EffectFade, Autoplay } from 'swiper/modules';
 register();
 const swiperEl = document.querySelector('swiper-container');
 if (swiperEl) {
   const params = {
+    modules: [Navigation, Pagination, EffectFade, Autoplay],
+    injectStylesUrls: [
+      './assets/navigation-element.css',
+      './assets/pagination-element.css',
+      './assets/autoplay-element.css',
+      './assets/effect-fade-element.css',
+    ],
     pagination: {
       clickable: true,
     }
