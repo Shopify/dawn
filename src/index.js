207b69f2ad647184801205c96f30c37c4faf395d
riku takenaka <riijon@gmail.com>
2023-11-07 13:41:55 +0900

    [update] 各種調整 ・不要なcssを削除 ・特集コレクションの表示アイテムがない場合は何も表示しない ・CPB ,DECORTEのスライダーをTOPと同様の実装に変更 ・ブランドページカテゴリー選択時のURLをfilter形式に

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 import './css/input.css'
 import './sass/app.scss'
-import { register } from 'swiper/element';
-import { Navigation, Pagination, EffectFade, Autoplay } from 'swiper/modules';
-register();
-const swiperEl = document.querySelector('swiper-container');
-if (swiperEl) {
-  const params = {
-    modules: [Navigation, Pagination, EffectFade, Autoplay],
-    injectStylesUrls: [
-      'https://cdn.jsdelivr.net/npm/swiper@10.3.1/modules/navigation-element.css',
-      'https://cdn.jsdelivr.net/npm/swiper@10.3.1/modules/pagination-element.css',
-      'https://cdn.jsdelivr.net/npm/swiper@10.3.1/modules/effect-fade-element.css',
-    ],
-    pagination: {
-      clickable: true,
-    }
-  }
+import {register} from 'swiper/element';
+import {Navigation, Pagination, EffectFade, Autoplay} from 'swiper/modules';
 
-  Object.assign(swiperEl, params)
-  swiperEl.initialize()
-}
+register();
+const swiperEls = document.querySelectorAll('swiper-container');
+
+swiperEls.forEach((swiperEl) => {
+  if (swiperEl) {
+    const params = {
+      modules: [Navigation, Pagination, EffectFade, Autoplay],
+      injectStylesUrls: [
+        'https://cdn.jsdelivr.net/npm/swiper@10.3.1/modules/navigation-element.css',
+        'https://cdn.jsdelivr.net/npm/swiper@10.3.1/modules/pagination-element.css',
+        'https://cdn.jsdelivr.net/npm/swiper@10.3.1/modules/effect-fade-element.css',
+      ],
+      pagination: {
+        clickable: true,
+      }
+    }
+
+    Object.assign(swiperEl, params)
+    swiperEl.initialize()
+  }
+})
