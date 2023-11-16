4f803385071aaaa990c0b9d0b784be5bdceb1721
Eugene Kasimov <105315663+eugenekasimov@users.noreply.github.com>
2023-11-16 11:48:56 -0800

    Quick Order Form: Follow Figma designs (#3072)
    * Quick Order Form scaffolding
    
    * [Refactoring] Replace loading spinner with snippet (#2996)
    
    * Change spinner for main-cart-items
    
    * Change spinner for cart-drawer
    
    * remove extra div and change classes
    
    * Replace spinner with snippet for pdp and fead prod
    
    * Remove unnecessary classes
    
    * Add comment for snippet
    
    * Change naming for files and css classes
    
    * Put back deleted elements
    
    * Remove unused classes
    
    * Put back loading-overlay for cases when no spinner
    
    * Chnage loading-overlay--error name
    
    * Minor change in CSS
    
    * Move loading-overlay styles to template-collection
    
    * Rename component-loading-overlay
    
    * Fix bug with overlay and spinner for facets
    
    * Remove component-loading-spinner import from files
    
    * Use spinner snippet for predictive search
    
    * Hide product count when loading for drawer filters
    
    * Address feedback. Clean-up.
    
    * Minor changes to address feedback
    
    * Fix spinner conflict with cart drawer
    
    * Address feedabck
    
    * Add missing whitespace for if tag
    
    * Quick Order Form scaffolding
    
    * Add styles to make form look according to Figma
    
    * Make sure QOL works properly
    
    * Styles for images
    
    * Remove price from product
    
    * Adjust css styles for the quick order form
    
    * Update section settings
    
    * Add filtering and search mockup
    
    * Remove settings for search and picking collections
    
    * Prevent being to added to all templates.
    Limit numer of the sections to 1.
    
    * Remove filter
    
    * Fix typo
    
    * Remove unnecessary styles and change px to rem
    
    * Simplify product title row
    
    * Roll back changes for quick-order-list-row
    
    * Add display flex to variant column for QOL
    
    * Remove empty row
    
    * Fix global settings for media
    
    * Add link to product title
    
    * Prevent border going over image size
    
    * Refactoring. Add hover effect while focusing.
    
    * Remove shadow from total bar
    
    * Fix visible empty space and outline for no-media
    
    * Remove unnecessary parameters for snippet
    
    * Minor changes in css
    
    * Remove hardcoded text
    
    * Update total bar count items and price
    
    * Change Product total to Subtotal. Add currency code.
    Fix shadow.
    
    * Change ID for quick-order-form search.
    Re order templates for enabled_on.
    
    ---------
    
    Co-authored-by: Sofia Matulis <sofia.matulis@shopify.com>

--- assets/quick-order-list.js
+++ assets/quick-order-list.js
@@ -67,8 +67,7 @@ class QuickOrderList extends HTMLElement {
       add: 'ADD',
       update: 'UPDATE'
     }
-    this.id = this.dataset.id
-    this.quickOrderListId = `${this.id}`
+    this.quickOrderListId = this.dataset.id;
     this.variantItemStatusElement = document.getElementById('shopping-cart-variant-item-status');
     const form = this.querySelector('form');
 
@@ -123,7 +122,7 @@ class QuickOrderList extends HTMLElement {
       .then((response) => response.text())
       .then((responseText) => {
         const html = new DOMParser().parseFromString(responseText, 'text/html');
-        const sourceQty = html.querySelector(this.quickOrderListId);
+        const sourceQty = html.querySelector(`#${this.quickOrderListId}`);
         this.innerHTML = sourceQty.innerHTML;
       })
       .catch(e => {
