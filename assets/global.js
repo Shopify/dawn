af3166fa00cfe603100301322ed2839c855e1225
Sofia Matulis <sofiamatulis@users.noreply.github.com>
2024-03-01 15:18:24 -0500

    Bulk Quick Add: Clicking "-" takes you back to 0 (#3297)

--- assets/global.js
+++ assets/global.js
@@ -231,16 +231,28 @@ class QuantityInput extends HTMLElement {
     event.preventDefault();
     const previousValue = this.input.value;
 
-    event.target.name === 'plus' ? this.input.stepUp() : this.input.stepDown();
+    if (event.target.name === 'plus') {
+      if ((parseInt(this.input.dataset.min) > parseInt(this.input.step)) && this.input.value == 0) {
+        this.input.value = this.input.dataset.min;
+      } else {
+        this.input.stepUp()
+      } 
+    } else {
+      this.input.stepDown();
+    }
+    
     if (previousValue !== this.input.value) this.input.dispatchEvent(this.changeEvent);
+
+    if ((this.input.dataset.min === previousValue) &&  event.target.name === 'minus') {
+      this.input.value = parseInt(this.input.min);
+    }
   }
 
   validateQtyRules() {
     const value = parseInt(this.input.value);
     if (this.input.min) {
-      const min = parseInt(this.input.min);
       const buttonMinus = this.querySelector(".quantity__button[name='minus']");
-      buttonMinus.classList.toggle('disabled', value <= min);
+      buttonMinus.classList.toggle('disabled', parseInt(value) <= parseInt(this.input.min));
     }
     if (this.input.max) {
       const max = parseInt(this.input.max);
