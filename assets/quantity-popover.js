2e13247cc5493a9222974f12f262647002edb347
Sofia Matulis <sofia.matulis@shopify.com>
2024-03-05 15:28:14 -0500

    Quick Add Bulk Popover

--- assets/quantity-popover.js
+++ assets/quantity-popover.js
@@ -10,30 +10,27 @@ if (!customElements.get('quantity-popover')) {
         this.infoButtonMobile = this.querySelector('.quantity-popover__info-button--icon-with-label');
         this.popoverInfo = this.querySelector('.quantity-popover__info');
         this.closeButton = this.querySelector('.button-close');
-        this.variantInfo = this.querySelector('.quantity-popover-container');
         this.eventMouseEnterHappened = false;
 
         if (this.closeButton) {
           this.closeButton.addEventListener('click', this.closePopover.bind(this));
         }
 
-        if (this.popoverInfo && this.infoButtonDesktop && this.mql.matches) {
-          this.popoverInfo.addEventListener('mouseenter', this.closePopover.bind(this));
+        if (this.popoverInfo && this.infoButtonDesktop && this.mqlTablet.matches) {
+          this.popoverInfo.addEventListener('mouseleave', this.closePopover.bind(this));
         }
 
         if (this.infoButtonDesktop) {
           this.infoButtonDesktop.addEventListener('click', this.togglePopover.bind(this));
-          this.infoButtonDesktop.addEventListener('focusout', this.closePopover.bind(this));
         }
 
         if (this.infoButtonMobile) {
           this.infoButtonMobile.addEventListener('click', this.togglePopover.bind(this));
-          this.infoButtonMobile.addEventListener('focusout', this.closePopover.bind(this));
         }
 
         if (this.infoButtonDesktop && this.mqlTablet.matches) {
-          this.variantInfo.addEventListener('mouseenter', this.togglePopover.bind(this));
-          this.variantInfo.addEventListener('mouseleave', this.closePopover.bind(this));
+          this.infoButtonDesktop.addEventListener('mouseenter', this.togglePopover.bind(this));
+          this.infoButtonDesktop.addEventListener('mouseleave', this.closePopover.bind(this));
         }
       }
 
@@ -55,19 +52,25 @@ if (!customElements.get('quantity-popover')) {
         const isOpen = button.getAttribute('aria-expanded') === 'true';
 
         button.classList.toggle('quantity-popover__info-button--open');
-
+      
         if (isOpen && event.type !== 'mouseenter') {
           button.focus();
+          button.addEventListener('keyup', (e) => {
+            if (e.key === 'Escape') {
+              this.closePopover(e)
+            }
+          })
         }
       }
 
       closePopover(event) {
         event.preventDefault();
-        const isChild = this.variantInfo.contains(event.relatedTarget);
+        const isButtonChild = this.infoButtonDesktop.contains(event.relatedTarget);
+        const isPopoverChild = this.popoverInfo.contains(event.relatedTarget)
 
         const button = this.infoButtonDesktop && this.mql.matches ? this.infoButtonDesktop : this.infoButtonMobile;
 
-        if (!event.relatedTarget || !isChild) {
+        if (!isButtonChild && !isPopoverChild) {
           button.setAttribute('aria-expanded', 'false');
           button.classList.remove('quantity-popover__info-button--open');
           this.popoverInfo.setAttribute('hidden', '');
