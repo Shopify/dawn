609435aec4b9bc5d7dc1a601f9a0ff4101e66039
Lars Hoffbeck <larshoffbeck@gmail.com>
2024-03-15 16:16:13 -0400

    Removed noscript and IE-specific code (#3245)
    Removed no-JS requirement from the contribution guidelines
    
    Remove CSS and no-js-only HTML
    
    add JS class to <html>
    
    remove noscript css selectors
    
    Update README

--- assets/facets.js
+++ assets/facets.js
@@ -268,8 +268,6 @@ class FacetFiltersForm extends HTMLElement {
       sortFilterForms.forEach((form) => {
         if (!isMobile) {
           if (form.id === 'FacetSortForm' || form.id === 'FacetFiltersForm' || form.id === 'FacetSortDrawerForm') {
-            const noJsElements = document.querySelectorAll('.no-js-list');
-            noJsElements.forEach((el) => el.remove());
             forms.push(this.createSearchParams(form));
           }
         } else if (form.id === 'FacetFiltersFormMobile') {
