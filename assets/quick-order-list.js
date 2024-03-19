7a5f3d34706a998a35842d901bd1a7ef3a9b13bf
Sofia Matulis <sofia.matulis@shopify.com>
2024-03-19 19:17:52 -0400

    Remove if

--- assets/quick-order-list.js
+++ assets/quick-order-list.js
@@ -155,7 +155,6 @@ class QuickOrderList extends HTMLElement {
   }
   
   validateQuantity(event, name, index, inputValue, cartQuantity, quantity) {
-    this.validateError = true
     if (inputValue < event.target.dataset.min) {
       event.target.setCustomValidity(window.quickOrderListStrings.min_error.replace('[min]', event.target.dataset.min));
       event.target.reportValidity();
@@ -171,7 +170,6 @@ class QuickOrderList extends HTMLElement {
     } else {
       event.target.setCustomValidity('');
       event.target.reportValidity();
-      this.validateError = false
       if (cartQuantity > 0) {
         this.updateQuantity(index, inputValue, name, this.actions.update);
       } else {
@@ -282,37 +280,35 @@ class QuickOrderList extends HTMLElement {
       return;
     }
 
-    if (this.validateError == false) {
-    this.variantListInput = event.target;
-    this.variantListInput.select()
-      if (this.allInputsArray.length !== 1) {
-        this.variantListInput.addEventListener('keydown', (e) => {
-          if (e.key === 'Enter') {
-            e.preventDefault();
-            e.target.blur();
-            const currentIndex = this.allInputsArray.indexOf(e.target);
-            if (!e.shiftKey) {
-              const nextIndex = currentIndex + 1;
-              const nextVariant = this.allInputsArray[nextIndex] || this.allInputsArray[0];
-              nextVariant.select();
-            } else {
-              const previousIndex = currentIndex - 1;
-              const previousVariant = this.allInputsArray[previousIndex] || this.allInputsArray[this.allInputsArray.length - 1];
-              previousVariant.select();
-            }
+  this.variantListInput = event.target;
+  this.variantListInput.select()
+    if (this.allInputsArray.length !== 1) {
+      this.variantListInput.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+          e.target.blur();
+          const currentIndex = this.allInputsArray.indexOf(e.target);
+          if (!e.shiftKey) {
+            const nextIndex = currentIndex + 1;
+            const nextVariant = this.allInputsArray[nextIndex] || this.allInputsArray[0];
+            nextVariant.select();
+          } else {
+            const previousIndex = currentIndex - 1;
+            const previousVariant = this.allInputsArray[previousIndex] || this.allInputsArray[this.allInputsArray.length - 1];
+            previousVariant.select();
           }
-        });
+        }
+      });
 
-        this.scrollQuickOrderListTable();
+      this.scrollQuickOrderListTable();
 
-      } else {
-        this.variantListInput.addEventListener('keydown', (e) => {
-          if (e.key === 'Enter') {
-            e.preventDefault();
-            e.target.blur();
-          }
-        });
-      }
+    } else {
+      this.variantListInput.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+          e.target.blur();
+        }
+      });
     }
   }
 
