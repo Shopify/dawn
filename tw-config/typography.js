26d06ee1238e79510e6795e367960af5606b1f77
iyanuoluwa sowande <iyanusowande@gmail.com>
2024-06-06 23:25:09 -0700

    comfigure design system colors spacing and typography

---
+++ tw-config/typography.js
@@ -0,0 +1,14 @@
+module.exports = {
+  headingStyles: {
+    lineHeight: '80%',
+    fontWeight: '700',
+  },
+  titleStyles: {
+    lineHeight: 'normal',
+    fontWeight: '500',
+  },
+  bodyStyles: {
+    lineHeight: 'normal',
+    fontWeight: '400',
+  },
+};
