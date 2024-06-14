c3b91af22f0dc32f08d82a629c7e8918b65838c5
Chris Haig <Chris@uncover.team>
2024-06-15 00:52:15 +0100

    mobile resposiveness

--- assets/blend.js
+++ assets/blend.js
@@ -1,3 +1,5 @@
+Window.values = {}
+
 const changeFlavour = (flavour) => {
     let blendDots = document.querySelectorAll('.blend-dot');
     // remove all active classes
@@ -43,6 +45,125 @@ const stepFlavour = (step) => {
     }
 }
 
+const addValue = (name) => {
+
+    if(totalValue() != 20){
+
+        if(Window.values[name]){
+            Window.values[name]++;
+        }else{
+            Window.values[name] = 1;
+        }
+
+    }
+
+    document.querySelector(`[name="${name}"]`).innerHTML = Window.values[name] + "cl";
+
+    combineColors()
+}
+
+const combineColors = () => {
+    let data = Window.data;
+
+    let colors = []
+
+    // full bottle is 20cl so its divided by 20
+    for (const key in Window.values) {
+        // console.log(key, Window.values[key]);
+        let flavour = data.find(flavour => flavour.title == key);
+        let colorHex = flavour.blendColour;
+        // convert Window.values[name] to percentage of 20
+        let percentage = (Window.values[key] / 20) * 100;
+        colors.push({colorHex, percentage});
+    }
+  
+    console.log(getWeightedColor(colors))
+    document.querySelector('.bottom-layer').style.background = `${getWeightedColor(colors)}`;
+    document.querySelector('.bottom-layer').style.height = `Calc(${(totalValue() * 3.5)}%)`;
+
+    if(totalValue() == 20){
+        document.querySelector('.label-layer').style.display = 'block';
+        document.querySelector('.cap-layer').style.display = 'block';
+    }else{
+        document.querySelector('.label-layer').style.display = 'none';
+        document.querySelector('.cap-layer').style.display = 'none';
+    }
+}
+
+function hexToRgb(hex) {
+    let bigint = parseInt(hex.slice(1), 16);
+    let r = (bigint >> 16) & 255;
+    let g = (bigint >> 8) & 255;
+    let b = bigint & 255;
+  
+    return [r, g, b];
+  }
+  
+  function rgbToHex(r, g, b) {
+    return (
+      "#" +
+      ((1 << 24) + (r << 16) + (g << 8) + b)
+        .toString(16)
+        .slice(1)
+        .toUpperCase()
+    );
+  }
+  
+  function getWeightedColor(colors) {
+    let totalPercentage = 0;
+    let weightedRgb = [0, 0, 0];
+  
+    colors.forEach(color => {
+      totalPercentage += color.percentage;
+    });
+  
+    colors.forEach(color => {
+      let weight = color.percentage / totalPercentage;
+      let [r, g, b] = hexToRgb(color.colorHex);
+  
+      weightedRgb[0] += r * weight;
+      weightedRgb[1] += g * weight;
+      weightedRgb[2] += b * weight;
+    });
+  
+    return rgbToHex(
+      Math.round(weightedRgb[0]),
+      Math.round(weightedRgb[1]),
+      Math.round(weightedRgb[2])
+    );
+  }
+
+const totalValue = () => {
+    let total = 0;
+    for (const key in Window.values) {
+        total += Window.values[key];
+    }
+    return total;
+}
+
+const decreaseValue = (value) => {
+
+    if(Window.values[value]){
+        if(Window.values[value] > 0){
+            Window.values[value]--;
+        }
+    }else{
+        Window.values[value] = 0;
+    }
+
+    document.querySelector(`[name="${value}"]`).innerHTML = Window.values[value] + "cl";
+
+    combineColors()
+}
+
+
+const handleBlendName = (e) => {
+    console.log("changing");
+    let name = e.value;
+    console.log("name, ", name);
+    document.querySelector('.text').innerHTML = name;
+
+}
 
 window.addEventListener('DOMContentLoaded', () => {
     changeFlavour(1);
