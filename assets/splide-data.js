84c03522761f78ae64398bab36faaa39b8fc9a9e
iyanuoluwa sowande <iyanusowande@gmail.com>
2024-06-18 23:05:01 -0700

    fix carousel

--- assets/splide-data.js
+++ assets/splide-data.js
@@ -2,12 +2,10 @@ const splide = new Splide('#main-carousel', {
   heightRatio: 1,
   cover: true,
   pagination: false,
-  width: '640px',
   loop: true,
   breakpoints: {
     800: {
       pagination: true,
-      width: '100vw',
     },
   },
 });
@@ -19,7 +17,6 @@ function initThumbnail(thumbnail, index) {
 }
 
 const thumbnails = document?.getElementsByClassName('thumbnail');
-const thumbnailContainer = document?.getElementsById('thumbnails');
 let current;
 
 for (let i = 0; i < thumbnails.length; i++) {
@@ -27,7 +24,6 @@ for (let i = 0; i < thumbnails.length; i++) {
 }
 
 splide.on('mounted move', function () {
-  console.log('Mounted');
   const thumbnail = thumbnails[splide.index];
 
   if (thumbnail) {
@@ -38,30 +34,7 @@ splide.on('mounted move', function () {
     thumbnail.classList.add('is-active');
     thumbnail.setAttribute('aria-current', 'true');
     current = thumbnail;
-    thumbnailContainer.scrollLeft = thumbnail.offsetLeft;
   }
 });
 
 splide.mount();
-
-// const related_carousel = new Splide('#related-products-carousel', {
-//   heightRatio: 1,
-//   cover: true,
-//   pagination: false,
-//   width: '320px',
-//   height: '444px',
-//   loop: true,
-//   breakpoints: {
-//     800: {
-//       width: '197px',
-//       height: '294px',
-//       // perPage:2
-//     },
-//   },
-// });
-
-// related_carousel.mount();
-
-// related_carousel.on('mount', () => {
-//   console.log('Mounted carousel');
-// });
