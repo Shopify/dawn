bc244af0e8bcdcdb6236b3bc0cc2088c9fca71b2
iyanuoluwa sowande <iyanusowande@gmail.com>
2024-07-11 18:53:43 -0700

    create related products carousel on homepage

---
+++ assets/splide-collection-carousel.js
@@ -0,0 +1,79 @@
+function disableCarouselArrows(carousel_id) {
+  const items = document?.querySelectorAll(`#${carousel_id} li.splide__slide`);
+  const firstElement = items?.[0];
+  const lastElement = items?.[items?.length - 1];
+
+  const prevBtn = document?.querySelector(`#${carousel_id} .splide__arrow--prev`);
+  const nextBtn = document?.querySelector(`#${carousel_id} .splide__arrow--next`);
+
+  return function () {
+    if (firstElement?.classList.contains('is-active')) {
+      prevBtn.setAttribute('disabled', true);
+    }
+
+    if (lastElement?.classList.contains('is-active')) {
+      nextBtn.setAttribute('disabled', true);
+    }
+  };
+}
+
+function createRelatedProducts(carouselId) {
+  const id = `#${carouselId}`;
+  try {
+    const related_carousel = new Splide(id, {
+      cover: true,
+      pagination: false,
+      perPage: 4,
+      perMove: 1,
+      focus: 0,
+      omitEnd: true,
+      drag: 'free',
+      snap: true,
+      autoWidth: true,
+      breakpoints: {
+        800: {
+          perPage: 4,
+        },
+        400: {
+          perPage: 2,
+        },
+      },
+    });
+
+    related_carousel.on('arrows:updated', disableCarouselArrows(carouselId));
+    related_carousel.mount();
+  } catch (error) {
+    console.error(error);
+    console.error(`Error mounting related carousel ${carouselId}`, error);
+  }
+}
+
+try {
+  const collectionCarousels = document.querySelectorAll('[id^="Collection-Carousel-"]');
+
+  collectionCarousels.forEach((carousel) => {
+    createRelatedProducts(carousel.id);
+
+    // const targetNode = document.querySelector('.related-products');
+    // const config = { childList: true, subtree: true };
+
+    // const callback = function (mutationsList, observer) {
+    //   for (let mutation of mutationsList) {
+    //     if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+    //       // Check if the added nodes include the carousel
+    //       if (document.querySelector('#related-products-carousel .splide__list')) {
+    //         createRelatedProducts();
+    //         observer.disconnect(); // Stop observing once the carousel is initialized
+    //         break;
+    //       }
+    //     }
+    //   }
+    // };
+
+    // const observer = new MutationObserver(callback);
+
+    // if (targetNode) {
+    //   observer.observe(targetNode, config);
+    // }
+  });
+} catch (error) {}
