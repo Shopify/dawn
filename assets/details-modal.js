54a0b1ef55225b9ddc990a8f5e17bbb62e96dc66
Josh Corbett <joshwcorbett@icloud.com>
2024-08-08 15:11:53 -0600

    docs: :memo: document `details-modal.js`

--- assets/details-modal.js
+++ assets/details-modal.js
@@ -1,7 +1,13 @@
+/**
+ * Details Modal custom element class.
+ * @extends HTMLElement
+ */
 class DetailsModal extends HTMLElement {
   constructor() {
     super();
+    /** @type {HTMLElement} */
     this.detailsContainer = this.querySelector('details');
+    /** @type {HTMLElement} */
     this.summaryToggle = this.querySelector('summary');
 
     this.detailsContainer.addEventListener('keyup', (event) => event.code.toUpperCase() === 'ESCAPE' && this.close());
@@ -11,19 +17,37 @@ class DetailsModal extends HTMLElement {
     this.summaryToggle.setAttribute('role', 'button');
   }
 
+  /**
+   * Return open state of modal.
+   * @returns {boolean} - True if modal is open, false otherwise.
+   */
   isOpen() {
     return this.detailsContainer.hasAttribute('open');
   }
 
+  /**
+   * Handle summary element click event.
+   * Toggles the open state of the modal.
+   * @param {Event} event - The click event object.
+   */
   onSummaryClick(event) {
     event.preventDefault();
     event.target.closest('details').hasAttribute('open') ? this.close() : this.open(event);
   }
 
+  /**
+   * Handle body click event.
+   * Closes the modal if click is outside of it.
+   * @param {Event} event - The click event object.
+   */
   onBodyClick(event) {
     if (!this.contains(event.target) || event.target.classList.contains('modal-overlay')) this.close(false);
   }
 
+  /**
+   * Open the modal and trap focus.
+   * @param {Event} event - The click event object.
+   */
   open(event) {
     this.onBodyClickEvent = this.onBodyClickEvent || this.onBodyClick.bind(this);
     event.target.closest('details').setAttribute('open', true);
@@ -36,6 +60,10 @@ class DetailsModal extends HTMLElement {
     );
   }
 
+  /**
+   * Close the modal and release focus.
+   * @param {boolean} [focusToggle=true] - Whether to focus the summary toggle element after closing.
+   */
   close(focusToggle = true) {
     removeTrapFocus(focusToggle ? this.summaryToggle : null);
     this.detailsContainer.removeAttribute('open');
