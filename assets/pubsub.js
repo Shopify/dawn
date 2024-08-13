95b73d8d593a410908a3aaa631f77428db94178e
Josh Corbett <joshwcorbett@icloud.com>
2024-08-13 11:06:22 -0600

    docs: :memo: document `pubsub.js`

--- assets/pubsub.js
+++ assets/pubsub.js
@@ -1,5 +1,15 @@
+/**
+ * List of subscribers for each event.
+ * @type {Object<string, Array<function>>}
+ */
 let subscribers = {};
 
+/**
+ * Subscribe to an event.
+ * @param {string} eventName - Name of event to subscribe to.
+ * @param {Function} callback - Callback function to execute on event.
+ * @returns {Function} Unsubscribe function.
+ */
 function subscribe(eventName, callback) {
   if (subscribers[eventName] === undefined) {
     subscribers[eventName] = [];
@@ -7,6 +17,7 @@ function subscribe(eventName, callback) {
 
   subscribers[eventName] = [...subscribers[eventName], callback];
 
+  /** Unsubscribe from an event. Remove callback from event name in subscribers array. */
   return function unsubscribe() {
     subscribers[eventName] = subscribers[eventName].filter((cb) => {
       return cb !== callback;
@@ -14,6 +25,11 @@ function subscribe(eventName, callback) {
   };
 }
 
+/**
+ * Publish an event.
+ * @param {string} eventName - Name of event to publish.
+ * @param {any} data - Data to pass to subscribers.
+ */
 function publish(eventName, data) {
   if (subscribers[eventName]) {
     subscribers[eventName].forEach((callback) => {
