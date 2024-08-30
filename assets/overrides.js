c8635f40c0c6ef027f008a595d9457de74b32543
Jacob Fisher <jfisher@basicagency.com>
2024-08-30 00:03:36 -0500

    Add option name labels

--- assets/overrides.js
+++ assets/overrides.js
@@ -72,7 +72,7 @@ function init() {
   subscribe(PUB_SUB_EVENTS.variantChange, function (e) {
     TNB.tallSizingControls(false);
 
-    // console.log('Variant Change:', e);
+    console.log('Variant Change:', e);
   });
 
   TNB.tallSizingControls(false);
