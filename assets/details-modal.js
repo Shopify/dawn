e8d6b0f5dd21c291498ea98dbdb95ddbc2d887fc
Jacob Fisher <jfisher@basicagency.com>
2024-09-04 23:34:54 -0500

    Hide header when modal opens

--- assets/details-modal.js
+++ assets/details-modal.js
@@ -3,6 +3,7 @@ class DetailsModal extends HTMLElement {
     super();
     this.detailsContainer = this.querySelector('details');
     this.summaryToggle = this.querySelector('summary');
+    this.header = document.querySelector('.section-header');
 
     this.detailsContainer.addEventListener('keyup', (event) => event.code.toUpperCase() === 'ESCAPE' && this.close());
     this.summaryToggle.addEventListener('click', this.onSummaryClick.bind(this));
@@ -29,6 +30,7 @@ class DetailsModal extends HTMLElement {
     event.target.closest('details').setAttribute('open', true);
     document.body.addEventListener('click', this.onBodyClickEvent);
     document.body.classList.add('overflow-hidden');
+    this.header.classList.add('shopify-section-header-hidden');
 
     trapFocus(
       this.detailsContainer.querySelector('[tabindex="-1"]'),
