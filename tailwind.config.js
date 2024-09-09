28d7b8df0d0d3f287605e9dd5cbeffa12de9fe6e
mahi9411 <mdagdi87@gmail.com>
2024-09-09 23:12:37 +0530

    anouncement bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@
 module.exports = {
   content: ['./**/*.liquid'],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
