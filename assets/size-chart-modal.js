0da1c39849728c668cf4e192e46262ef5c0bfa1d
Jacob Fisher <jfisher@basicagency.com>
2024-09-09 21:57:33 -0500

    Size chart cleanup

--- assets/size-chart-modal.js
+++ assets/size-chart-modal.js
@@ -35,10 +35,12 @@ document.addEventListener('DOMContentLoaded', () => {
   $('body').on('keyup', (event) => {
     if (event.key.toUpperCase() === 'ESCAPE') {
       var sizeChartModalDetails = $('.size-chart-modal details');
-      console.log(sizeChartModalDetails);
+
+      console.log(sizeChartModalDetails.eq(0), sizeChartModalDetails.eq(0).hasAttribute('open'));
 
       if (sizeChartModalDetails.eq(0).hasAttribute('open')) {
         var sizeChartToggle = $('.size-chart__toggle');
+
         sizeChartModalDetails.removeAttr('open');
         removeTrapFocus(sizeChartToggle.eq(0));
         document.body.classList.remove('overflow-hidden');
