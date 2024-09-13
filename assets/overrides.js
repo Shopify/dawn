aa3353ee870cbb8bfb0bf5b54441edc38938e532
Jacob Fisher <jfisher@basicagency.com>
2024-09-13 16:25:13 -0500

    Rework tall sizing controls

--- assets/overrides.js
+++ assets/overrides.js
@@ -22,78 +22,70 @@ var TNB = {
       });
     });
   },
-  tallSizingControls: function (destroy) {
-    var controls = document.querySelector('.tall-sizing-controls');
-
-    if (controls) {
+  tallSizingControls: function () {
+    function handleClickControl(controls, tall) {
       var regularOptions = controls.parentNode.querySelectorAll('label:not([data-tall-size])');
       var tallOptions = controls.parentNode.querySelectorAll('label[data-tall-size]');
       var buttonRegular = controls.querySelector('[data-button-regular]');
       var buttonTall = controls.querySelector('[data-button-tall]');
 
-      function handleClickControl(tall) {
-        regularOptions.forEach(function (el) {
-          el.classList.add('hide');
-        });
+      regularOptions.forEach(function (el) {
+        el.classList.add('hide');
+      });
+      tallOptions.forEach(function (el) {
+        el.classList.add('hide');
+      });
+
+      buttonRegular.classList.add('inactive');
+      buttonTall.classList.add('inactive');
+      TNB.state.tallSizingActive = false;
+
+      if (tall) {
         tallOptions.forEach(function (el) {
-          el.classList.add('hide');
+          el.classList.remove('hide');
         });
 
-        buttonRegular.classList.add('inactive');
-        buttonTall.classList.add('inactive');
+        buttonTall.classList.remove('inactive');
 
-        if (tall) {
-          tallOptions.forEach(function (el) {
-            el.classList.remove('hide');
-          });
-
-          buttonTall.classList.remove('inactive');
-
-          TNB.state.tallSizingActive = true;
-        } else {
-          regularOptions.forEach(function (el) {
-            el.classList.remove('hide');
-          });
-
-          buttonRegular.classList.remove('inactive');
-
-          TNB.state.tallSizingActive = false;
-        }
-      }
-
-      if (destroy) {
-        buttonRegular.removeEventListener('click', function () {
-          handleClickControl(false);
-        });
-        buttonTall.removeEventListener('click', function () {
-          handleClickControl(true);
-        });
+        TNB.state.tallSizingActive = true;
       } else {
-        buttonRegular.addEventListener('click', function () {
-          handleClickControl(false);
-        });
-        buttonTall.addEventListener('click', function () {
-          handleClickControl(true);
+        regularOptions.forEach(function (el) {
+          el.classList.remove('hide');
         });
 
-        if (TNB.state.tallSizingActive) {
-          buttonTall.click();
-        }
+        buttonRegular.classList.remove('inactive');
       }
+    }
+
+    $('body').on('click', '.tall-sizing-controls [data-button-regular]', function () {
+      const target = $(e.currentTarget);
+      const controls = target.closest('.tall-sizing-controls').get(0);
+      handleClickControl(controls, false);
+    });
+
+    $('body').on('click', '.tall-sizing-controls [data-button-tall]', function () {
+      const target = $(e.currentTarget);
+      const controls = target.closest('.tall-sizing-controls').get(0);
+      handleClickControl(controls, true);
+    });
+  },
+  refreshTallSizingControls: function () {
+    if (TNB.state.tallSizingActive) {
+      $('[data-button-tall]').click();
     }
   },
 };
 
 function init() {
   subscribe(PUB_SUB_EVENTS.optionValueSelectionChange, function (e) {
-    TNB.tallSizingControls(true);
+    TNB.refreshTallSizingControls();
   });
 
-  subscribe(PUB_SUB_EVENTS.variantChange, function (e) {
-    TNB.tallSizingControls(false);
-  });
+  // subscribe(PUB_SUB_EVENTS.variantChange, function (e) {
+  //   TNB.tallSizingControls(false);
+  // });
 
-  TNB.tallSizingControls(false);
+  TNB.tallSizingControls();
 
   TNB.megaMenuHover();
 }
