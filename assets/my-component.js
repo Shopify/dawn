cdc19fbc4f40ff01a47c7d0d0f5f58879ec4b2cd
Prateek Saini <psaini@radicleinc.com>
2024-09-27 17:48:45 +0530

    Updated

--- assets/my-component.js
+++ assets/my-component.js
@@ -2,3 +2,13 @@ Vue.component('my-component', {
   template: '<div>Hello from My Component!</div>',
   // Add your component's data, methods, etc. here
 });
+
+const app2 = Vue.createApp({
+  data() {
+    return {
+      message: 'Hello World!',
+    };
+  },
+});
+
+app.mount('#app2');
