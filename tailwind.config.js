e29ba5573bf7f437c5038b4f53cfdc313c104e4c
vlad_haleta <galeta.vlad@gmail.com>
2024-10-29 16:46:05 +0200

    add custom product

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.liquid', './**/*.html', './**/*.js', './**/*.css'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
