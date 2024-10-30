2d20ee77ce06bd688ff0ad5ef964e2786c3bdab6
jacobfisherr <jcbfshr@gmail.com>
2024-10-29 21:22:06 -0500

    Add popup notice to product page

---
+++ assets/popup-notice.js
@@ -0,0 +1,66 @@
+class CustomPopup extends HTMLElement {
+  constructor() {
+    super();
+    this.closeButton = this.querySelector('.close-popup');
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `<slot></slot>`;
+    this.isOpen = false;
+
+    // Check sessionStorage to see if popup has already been closed
+    if (!sessionStorage.getItem('popupClosed')) {
+      this.open();
+    }
+
+    this.closeButton.addEventListener('click', this.close.bind(this));
+    document.addEventListener('keydown', this.handleKeydown.bind(this));
+  }
+
+  open() {
+    this.setAttribute('open', '');
+    this.setAttribute('aria-hidden', 'false');
+    this.isOpen = true;
+    this.focus();
+    this.focusTrap();
+  }
+
+  close() {
+    this.removeAttribute('open');
+    this.setAttribute('aria-hidden', 'true');
+    this.isOpen = false;
+
+    // Store the popup closed state in sessionStorage
+    sessionStorage.setItem('popupClosed', 'true');
+  }
+
+  focusTrap() {
+    const focusableElements = this.querySelectorAll(
+      'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+    );
+    const firstFocusable = focusableElements[0];
+    const lastFocusable = focusableElements[focusableElements.length - 1];
+
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Tab') {
+        if (e.shiftKey) {
+          if (document.activeElement === firstFocusable) {
+            e.preventDefault();
+            lastFocusable.focus();
+          }
+        } else {
+          if (document.activeElement === lastFocusable) {
+            e.preventDefault();
+            firstFocusable.focus();
+          }
+        }
+      }
+    });
+  }
+
+  handleKeydown(e) {
+    if (e.key === 'Escape' && this.isOpen) {
+      this.close();
+    }
+  }
+}
+
+customElements.define('popup-notice', CustomPopup);
