4aee5cdc6d2f42e043667af9b2cf429d5482e823
jacobfisherr <jcbfshr@gmail.com>
2024-10-30 22:24:36 -0500

    Check for sections when rendering

--- assets/cart-notification.js
+++ assets/cart-notification.js
@@ -36,7 +36,7 @@ class CartNotification extends HTMLElement {
 
   renderContents(parsedState) {
     this.cartItemKey = parsedState.key;
-    if (pasrsedState.sections) {
+    if (parsedState.sections) {
       this.getSectionsToRender().forEach((section) => {
         document.getElementById(section.id).innerHTML = this.getSectionInnerHTML(
           parsedState.sections[section.id],
