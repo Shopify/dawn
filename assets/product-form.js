03fd9ae54568a8d4f22a8856cbe60699b3291862
jacobfisherr <jcbfshr@gmail.com>
2024-10-30 22:01:51 -0500

    Testing

--- assets/product-form.js
+++ assets/product-form.js
@@ -53,6 +53,8 @@ if (!customElements.get('product-form')) {
         }
         config.body = formData;
 
+        console.log(formData);
+
         fetch(`${routes.cart_add_url}`, config)
           .then((response) => response.json())
           .then((response) => {
