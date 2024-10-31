d6391230def785a8db382b8d46b7ce1dc0d6aa61
jacobfisherr <jcbfshr@gmail.com>
2024-10-30 22:07:47 -0500

    Testing

--- assets/cart-notification.js
+++ assets/cart-notification.js
@@ -36,14 +36,13 @@ class CartNotification extends HTMLElement {
 
   renderContents(parsedState) {
     this.cartItemKey = parsedState.key;
-
-    parsedState.sections &&
-      this.getSectionsToRender().forEach((section) => {
-        document.getElementById(section.id).innerHTML = this.getSectionInnerHTML(
-          parsedState.sections[section.id],
-          section.selector
-        );
-      });
+    this.getSectionsToRender().forEach((section) => {
+      console.log(section);
+      document.getElementById(section.id).innerHTML = this.getSectionInnerHTML(
+        parsedState.sections[section.id],
+        section.selector
+      );
+    });
 
     if (this.header) this.header.reveal();
     this.open();
