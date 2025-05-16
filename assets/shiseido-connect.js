3b54cd68b18b947aaf1542c3e9d0407467203e83
riku takenaka <riijon@gmail.com>
2025-05-16 18:04:13 +0900

    feature: SHISEIDO ID連携のAPIエンドポイントとスクリプト名を更新

---
+++ assets/shiseido-connect.js
@@ -0,0 +1,64 @@
+const cpbApiBaseUrl = document.querySelector('[cpb-api-base-url]').getAttribute('cpb-api-base-url');
+const cpbApiSiteSerial = document.querySelector('[cpb-api-site-serial]').getAttribute('cpb-api-site-serial');
+const cpbApiCheckIdUrl = document.querySelector('[cpb-api-check-id-url]').getAttribute('cpb-api-check-id-url');
+const cpbApiGetOtpUrl = document.querySelector('[cpb-api-get-otp-url]').getAttribute('cpb-api-get-otp-url');
+const customerId = __st.cid;
+
+async function cpbConnectCheck() {
+  const url = `${cpbApiCheckIdUrl}`;
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      site_serial: cpbApiSiteSerial,
+      tkisk_cd: '606222',
+      tkisk_ec_id: customerId,
+      target_url: cpbApiConnectUrl,
+    }),
+  });
+
+  const data = await response.json();
+
+  if (data.id_renkei_kbn === '2') {
+    const statusElements = document.querySelectorAll('.cpb-connect-status');
+    statusElements.forEach((element) => {
+      element.classList.remove('tw-hidden');
+    });
+  } else {
+    const statusElements = document.querySelectorAll('.cpb-connect-status-not-connected');
+    statusElements.forEach((element) => {
+      element.classList.remove('tw-hidden');
+    });
+  }
+
+  return data.id_renkei_kbn;
+}
+
+async function getCpbConnectOtp() {
+  const url = `${cpbApiGetOtpUrl}`;
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      site_serial: cpbApiSiteSerial,
+      tkisk_cd: '606222',
+      tkisk_ec_id: customerId,
+      target_url: cpbApiGetOtpUrl,
+    }),
+  });
+
+  const data = await response.json();
+  const aTags = document.querySelectorAll('a.cpb-connect-register');
+  aTags.forEach((aTag) => {
+    aTag.href += data.onetime_password;
+  });
+
+  return data.onetime_password;
+}
+
+cpbConnectCheck();
+getCpbConnectOtp();
