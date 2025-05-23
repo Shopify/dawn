74c32fc8d250424a13979c0f57674cdbc1ffd0b0
Jim Hockless <jim.hockless@flauntdigital.com>
2025-05-23 13:55:16 +0100

    added temporary cdn to get parallax working

--- assets/section-our-history.js
+++ assets/section-our-history.js
@@ -1,7 +1,6 @@
-document.addEventListener('scroll', () => {
-  const offset = window.scrollY * 0.02; // tweak for parallax strength
-  const target = document.querySelector('.media-grid__item--2 img');
-  if (target) {
-    target.style.transform = `translateY(${offset}px)`;
+document.addEventListener('DOMContentLoaded', function () {
+  if (typeof simpleParallax !== 'undefined') {
+    const images = document.getElementById('parallax-image');
+    new simpleParallax(images, { scale: 1.2, delay: 1.5, orientation: 'down' });
   }
 });
