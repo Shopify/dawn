049bd6fef364d0b3853e8b9ee11aca640c286a98
Supreme <siya360@gmail.com>
2026-07-07 19:07:03 +0200

    Remove old root-level Dawn theme attempt

--- assets/constants.js
+++
@@ -1,9 +0,0 @@
-const ON_CHANGE_DEBOUNCE_TIMER = 300;
-
-const PUB_SUB_EVENTS = {
-  cartUpdate: 'cart-update',
-  quantityUpdate: 'quantity-update',
-  optionValueSelectionChange: 'option-value-selection-change',
-  variantChange: 'variant-change',
-  cartError: 'cart-error',
-};
